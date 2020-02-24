018810ce8e532efb60ff90cfdd3c6221994172a2
Saul Feliz <646634@gmail.com>
2020-02-24 13:45:43 -0600

    HackerRank Arrays Left rotation problem completed https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays

---
+++ arrays/rotateLeft.js
@@ -0,0 +1,28 @@
+// A left rotation operation on an array shifts each of the array's elements 1 unit to the left. For example, if 2 left rotations are performed on array [1,2,3,4,5] , then the array would become [3,4,5,1,2] .
+
+// Given an array a of n integers and a number, d, perform d left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+// Function Description
+
+// Complete the function rotLeft in the editor below. It should return the resulting array of integers.
+
+// rotLeft has the following parameter(s):
+
+// An array of integers .
+// An integer , the number of rotations.
+
+// Complete the rotLeft function below.
+
+const a = [1, 2, 3, 4, 5];
+const d = 2;
+
+function rotLeft(a, d) {
+  while (d) {
+    a.push(a.shift());
+    d--;
+  }
+  console.log(a);
+  return a;
+}
+
+rotLeft(a, d);
