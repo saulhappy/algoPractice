4cb4dbaf7aa9dfafbeede3fa8d93e265202b08dd
Saul Feliz <646634@gmail.com>
2020-03-14 13:12:57 -0500

    added bonApetit HackerRank problem

---
+++ variousProblems/bonApetit.js
@@ -0,0 +1,4 @@
+//HackerRank: https://www.hackerrank.com/challenges/bon-appetit/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+// Complete the bonAppetit function below.
+function bonAppetit(bill, k, b) {}
