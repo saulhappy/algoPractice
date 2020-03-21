f37392aba57fcf16f925e186973839c92275034f
Saul Feliz <646634@gmail.com>
2020-03-21 18:07:42 -0500

    updated twoSum problem- reduced space

--- JavaScript/variousProblems/twoSum.js
+++ JavaScript/variousProblems/twoSum.js
@@ -16,17 +16,16 @@ const target = 10;
 function twoSum(nums, target) {
   // using brute force- (O)n sq time:
 
-  //   let result = [];
-  //   for (let i = 0; i < nums.length; i++) {
-  //     for (let j = i + 1; j < nums.length; j++) {
-  //       if (nums[i] + nums[j] === target) {
-  //         result.push(i, j);
-  //         return result;
-  //       }
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) {
+  //       return [i, j];
   //     }
   //   }
+  // }
 
   // one iteration and Hash Map O(n) time
+
   let numObj = {};
   for (let i = 0; i < nums.length; i++) {
     let complement = target - nums[i];
