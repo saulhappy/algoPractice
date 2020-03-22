ae1c90a06ff248150429f7bdbbe75ccdf828c029
Saul Feliz <646634@gmail.com>
2020-03-22 09:51:31 -0500

    completed mergeSort

--- JavaScript/sorting/mergeSort.js
+++ JavaScript/sorting/mergeSort.js
@@ -1,11 +1,20 @@
-const left = [-30, 22];
-const right = [0, 97];
+const arr = [-30, 98, 3, 34];
+
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
 
 function merge(left, right) {
   const results = [];
 
   while (left.length && right.length) {
-    debugger;
     if (left[0] < right[0]) {
       results.push(left.shift());
     } else {
@@ -15,4 +24,4 @@ function merge(left, right) {
   return [...results, ...left, ...right];
 }
 
-console.log(merge(left, right));
+console.log(mergeSort(arr));
