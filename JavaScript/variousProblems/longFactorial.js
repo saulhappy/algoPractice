e37306e35950b6a02fb1d6407ba93c95162d965a
Saul Feliz <646634@gmail.com>
2020-03-25 18:59:37 -0500

    first pass at long factorial algorithm

---
+++ JavaScript/variousProblems/longFactorial.js
@@ -0,0 +1,10 @@
+// source: https://www.hackerrank.com/challenges/extra-long-factorials/problem
+
+const n = 25;
+// result = 15511210043330985984000000; 25x24x23x22...1
+
+//algorithm: while n, add the multiplication of n - 1 to the result
+
+function extraLongFactorials(n) {}
+
+console.log(extraLongFactorials(n));
