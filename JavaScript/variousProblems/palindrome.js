ac3df950c92b728e40e5e7a2a5a658a283cb439d
Saul Feliz <646634@gmail.com>
2020-03-24 20:48:49 -0500

    completed palindrome problem

---
+++ JavaScript/variousProblems/palindrome.js
@@ -0,0 +1,22 @@
+// Directions
+// Given a string, return true if the string is a palindrome or false if it is not.
+// Palindromes are strings that form the same word if it is reversed.
+// *Do* include spaces and punctuation in determining if the string is a palindrome.
+// Examples:
+// palindrome("abba") === true
+// palindrome("abcdefg") === false
+
+const str = "abba";
+
+function palindrome(str) {
+  let revStr = "";
+  for (let i of str) {
+    revStr = i + revStr;
+  }
+  if (revStr === str) {
+    return true;
+  }
+  return false;
+}
+
+console.log(palindrome(str));
