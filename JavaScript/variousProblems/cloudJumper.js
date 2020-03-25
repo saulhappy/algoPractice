efd884d84333e5637e3bfd3031753be6d4023d63
Saul Feliz <646634@gmail.com>
2020-03-25 18:52:14 -0500

    first pass as cloudjumper algorithm

---
+++ JavaScript/variousProblems/cloudJumper.js
@@ -0,0 +1,10 @@
+// source: https://www.hackerrank.com/challenges/counting-valleys/forum?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+const c = [0, 1, 0, 0, 0, 1, 0];
+
+// algorithm: for each step of c, if i + 3 = 0, then jump to 3.
+// if i + 2 = 1, then pass
+
+function jumpingOnClouds(c) {}
+
+console.log(jumpingOnClouds(c));
