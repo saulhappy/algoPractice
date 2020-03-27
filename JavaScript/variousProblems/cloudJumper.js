c45faabd44d707e6a979d6fe937863ee12dac5e1
Saul Feliz <646634@gmail.com>
2020-03-26 21:22:22 -0500

    refactored cloudJumper to forget last step. all tests passed.

--- JavaScript/variousProblems/cloudJumper.js
+++ JavaScript/variousProblems/cloudJumper.js
@@ -3,16 +3,25 @@
 const c = [0, 0, 1, 0, 0, 1, 0];
 // result: 4
 
+// const c = [0, 0, 0, 0, 1, 0];
+// result: 3
+
+// const c = [0, 0, 0, 0];
+// result 3
+
 function jumpingOnClouds(c) {
   let jumps = 0;
-  let i = 1;
-  while (i < c.length) {
+  let i = 0;
+  while (i < c.length - 1) {
+    debugger;
     if (c[i + 2] != 1) {
       i += 2;
       jumps++;
-    } else {
+    } else if (c[i + 1] != 1) {
       i += 1;
       jumps++;
+    } else {
+      i++;
     }
   }
   return jumps;
