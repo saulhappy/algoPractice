1558f24aaf3add6a4e98c5daaedd81a1e792a543
Saul Feliz <646634@gmail.com>
2020-03-29 13:13:40 -0500

    climbingLeaderboard problem added

---
+++ JavaScript/variousProblems/climbingLeaderboard.js
@@ -0,0 +1,8 @@
+//source: https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
+
+const scores = [100, 100, 50, 40, 40, 20, 10];
+const alice = [5, 25, 50, 120];
+
+function climbingLeaderboard(scores, alice) {}
+
+console.log(climbingLeaderboard(scores, alice));
