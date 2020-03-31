fd21a57ee2a61b13a3a5b33fd83385701620584e
Saul Feliz <646634@gmail.com>
2020-03-31 15:17:50 -0500

    first pass at climbingLeaderboard problem

--- JavaScript/variousProblems/climbingLeaderboard.js
+++ JavaScript/variousProblems/climbingLeaderboard.js
@@ -3,6 +3,27 @@
 const scores = [100, 100, 50, 40, 40, 20, 10];
 const alice = [5, 25, 50, 120];
 
-function climbingLeaderboard(scores, alice) {}
+function climbingLeaderboard(scores, alice) {
+  // create scoresMap
+  let uniqueScores = [...new Set(scores)];
+  let scoresMap = {};
+  let uniqueAlice = [...new Set(alice)];
+  let aliceRank = [];
+  for (let i = 0; i < uniqueScores.length; i++) {
+    scoresMap[i + 1] = uniqueScores[i];
+  }
+
+  // for each element of alice, if the value of i exist in the map, push that score's key (rank) into
+  // the alice rank. else, iterate through the maps keys, to see what the rank ought to be.
+
+  for (let i = 0; i < uniqueAlice.length; i++) {
+    if (Object.values(scoresMap).includes(uniqueAlice[i])) {
+      aliceRank.push(
+        Object.keys(scoresMap).find(key => scoresMap[key] === uniqueAlice[i])
+      );
+    }
+  }
+  return aliceRank;
+}
 
 console.log(climbingLeaderboard(scores, alice));
