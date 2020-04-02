6223ba590c20c626359fad558f3cede66c0a6650
Saul Feliz <646634@gmail.com>
2020-04-02 18:53:42 -0500

    climbingLeaderboard passed first test

--- JavaScript/variousProblems/climbingLeaderboard.js
+++ JavaScript/variousProblems/climbingLeaderboard.js
@@ -2,59 +2,43 @@
 
 const scores = [100, 100, 50, 40, 40, 20, 10];
 const alice = [5, 25, 50, 120];
+// const globalUnique = [...new Set(scores)];
+
+// globalUnique = [100, 50, 40, 20, 10]
 
 function climbingLeaderboard(scores, alice) {
-  let scoresRank = [];
-  scoresRank[0] = 1;
+  let result = [];
+  let uniqueScores = [...new Set(scores)];
 
-  for (let i = 1; i < scores.length; i++) {
-    // debugger;
-    if (scores[i] === scores[i - 1]) {
-      scoresRank.push(scoresRank[i - 1]);
+  for (const score of alice) {
+    if (score >= uniqueScores[0]) {
+      result.push(1);
+    } else if (score <= uniqueScores[uniqueScores.length - 1]) {
+      result.push(uniqueScores.length + 1);
+    } else {
+      result.push(indexBinarySearch(score, uniqueScores, [...uniqueScores]));
     }
-    scoresRank.push(scoresRank[i] + 1);
+  }
+  return result;
+}
+
+function indexBinarySearch(score, uniqueScores, globalUnique) {
+  let mid = Math.floor(uniqueScores.length / 2);
+
+  // base case
+  if (score === uniqueScores[mid]) {
+    return mid + 1;
+  } else if (uniqueScores.length === 1 && uniqueScores[0] > score) {
+    return 2 + globalUnique.indexOf(uniqueScores[0]);
+  } else if (uniqueScores.length === 1 && uniqueScores[0] < score) {
+    return 1 + globalUnique.indexOf(uniqueScores[0]);
   }
 
-  return scoresRank;
+  if (score < uniqueScores[mid]) {
+    return indexBinarySearch(score, uniqueScores.slice(mid), globalUnique);
+  } else if (score > uniqueScores[mid]) {
+    return indexBinarySearch(score, uniqueScores.slice(0, mid), globalUnique);
+  }
 }
 
 console.log(climbingLeaderboard(scores, alice));
-
-// //source: https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
-
-// const scores = [100, 100, 50, 40, 40, 20, 10];
-// const alice = [5, 25, 50, 120];
-
-// function climbingLeaderboard(scores, alice) {
-//   // create scoresMap
-//   let uniqueScores = [...new Set(scores)];
-//   let scoresMap = {};
-//   let uniqueAlice = [...new Set(alice)];
-//   let aliceRank = [];
-//   for (let i = 0; i < uniqueScores.length; i++) {
-//     scoresMap[i + 1] = uniqueScores[i];
-//   }
-
-//   // for each element of alice, if the value of i exist in the map, push that score's key (rank) into
-//   // the alice rank. else, iterate through the maps keys, to see what the rank ought to be.
-
-//   for (let i = 0; i < uniqueAlice.length; i++) {
-//     if (Object.values(scoresMap).includes(uniqueAlice[i])) {
-//       aliceRank.push(
-//         Object.keys(scoresMap).find(key => scoresMap[key] === uniqueAlice[i])
-//       );
-//     } else {
-//       for (score of uniqueAlice) {
-//         for (value in scoresMap) {
-//           if (uniqueAlice[score] > scoresMap[value]) {
-//             scoresMap[value - 1] = uniqueAlice[score];
-//           }
-//           scoresMap[value] = uniqueAlice[score];
-//         }
-//       }
-//     }
-//   }
-//   return scoresMap;
-// }
-
-// console.log(climbingLeaderboard(scores, alice));
