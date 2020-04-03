3e460d1ef3854691d5308063c91c3848f273eccc
Saul Feliz <646634@gmail.com>
2020-04-03 09:43:17 -0500

    added leetcode 30 day problem:  happyNum

--- JavaScript/variousProblems/climbingLeaderboard.js
+++ JavaScript/variousProblems/climbingLeaderboard.js
@@ -16,29 +16,32 @@ function climbingLeaderboard(scores, alice) {
     } else if (score <= uniqueScores[uniqueScores.length - 1]) {
       result.push(uniqueScores.length + 1);
     } else {
-      result.push(indexBinarySearch(score, uniqueScores, uniqueScores));
+      result.push(indexBinarySearch(score, uniqueScores));
     }
   }
   return result;
 }
 
-function indexBinarySearch(score, uniqueScores, globalUnique) {
-  let mid = Math.floor(uniqueScores.length / 2);
-
-  // base case
-  if (score === uniqueScores[mid]) {
-    return mid + 1;
-  } else if (uniqueScores.length === 1 && uniqueScores[0] > score) {
-    return 2 + globalUnique.indexOf(uniqueScores[0]);
-  } else if (uniqueScores.length === 1 && uniqueScores[0] < score) {
-    return 2 + globalUnique.indexOf(uniqueScores[0]);
-  }
-
-  if (score < uniqueScores[mid]) {
-    return indexBinarySearch(score, uniqueScores.slice(mid), globalUnique);
-  } else if (score > uniqueScores[mid]) {
-    return indexBinarySearch(score, uniqueScores.slice(0, mid), globalUnique);
-  }
-}
+function indexBinarySearch(score, uniqueScores) {}
 
 console.log(climbingLeaderboard(scores, alice));
+
+// function works, but times out on tests. refactoring above.
+// function indexBinarySearch(score, uniqueScores, globalUnique) {
+//     let mid = Math.floor(uniqueScores.length / 2);
+
+//     // base case
+//     if (score === uniqueScores[mid]) {
+//       return mid + 1;
+//     } else if (uniqueScores.length === 1 && uniqueScores[0] > score) {
+//       return 2 + globalUnique.indexOf(uniqueScores[0]);
+//     } else if (uniqueScores.length === 1 && uniqueScores[0] < score) {
+//       return 2 + globalUnique.indexOf(uniqueScores[0]);
+//     }
+
+//     if (score < uniqueScores[mid]) {
+//       return indexBinarySearch(score, uniqueScores.slice(mid), globalUnique);
+//     } else if (score > uniqueScores[mid]) {
+//       return indexBinarySearch(score, uniqueScores.slice(0, mid), globalUnique);
+//     }
+//   }
