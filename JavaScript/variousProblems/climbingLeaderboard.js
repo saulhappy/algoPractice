b4ab65aea9956c38e7ebe20a1e8451f9fd6be34f
Saul Feliz <646634@gmail.com>
2020-04-06 11:59:18 -0500

    refactor climbingLeadorboard for efficiency gain

--- JavaScript/variousProblems/climbingLeaderboard.js
+++ JavaScript/variousProblems/climbingLeaderboard.js
@@ -2,7 +2,7 @@
 
 const scores = [100, 90, 90, 80, 75, 60];
 const alice = [50, 65, 77, 90, 102];
-//answer = [6,5,4,2,1]
+//expected result = [6,5,4,2,1]
 
 function climbingLeaderboard(scores, alice) {
   let result = [];
@@ -11,7 +11,7 @@ function climbingLeaderboard(scores, alice) {
   for (const score of alice) {
     if (score >= uniqueScores[0]) {
       result.push(1);
-    } else if (score <= uniqueScores[uniqueScores.length - 1]) {
+    } else if (score < uniqueScores[uniqueScores.length - 1]) {
       result.push(uniqueScores.length + 1);
     } else {
       result.push(rankBinarySearch(score, uniqueScores));
@@ -26,13 +26,14 @@ function rankBinarySearch(score, uniqueScores) {
 
   while (true) {
     let mid = Math.floor((start + end) / 2);
-    // debugger;
 
     // base cases
     if (uniqueScores[mid] === score) {
       return mid + 1;
     } else if (uniqueScores[mid] > score && uniqueScores[mid + 1] < score) {
       return mid + 2;
+    } else if (uniqueScores[mid] < score && uniqueScores[mid - 1 > score]) {
+      return mid - 1;
     }
 
     // recursion
