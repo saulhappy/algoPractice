85f32e218072219f448c2ccf063d0b2cef2572b0
Saul Feliz <646634@gmail.com>
2020-04-14 11:12:45 -0500

    added method 2 to isUnique problem

--- JavaScript/variousProblems/isUnique.js
+++ JavaScript/variousProblems/isUnique.js
@@ -1,20 +1,37 @@
 // Source: Cracking the coding interview, page 90.
 // Implement an algorithm to determine if all characters in a string are unique. What if you couldn't use an additional data structure?
 
-const str = "saul";
+const str = "saaul";
 
 // method 1: no additional data structure.
-function isUnique(str) {
-  let cleansed = str.split("").sort().join("");
+// function isUnique(str) {
+//   let cleansed = str.split("").sort().join("");
 
-  for (let i = 0; i < cleansed.length; i++) {
-    for (let j = i + 1; j < cleansed.length; j++) {
-      if (cleansed[i] === cleansed[j]) {
-        return false;
-      }
-    }
-    return true;
+//   for (let i = 0; i < cleansed.length; i++) {
+//     for (let j = i + 1; j < cleansed.length; j++) {
+//       if (cleansed[i] === cleansed[j]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// method 2: using a hash table
+
+function isUnique(str) {
+  let hash = {};
+
+  for (char of str) {
+    hash[char] = hash[char] + 1 || 1;
   }
+
+  for (char in hash) {
+    if (hash[char] > 1) {
+      return false;
+    }
+  }
+  return true;
 }
 
 console.log(isUnique(str));
