76b5b733d416b9bd3ed1eba08d443b6dd3ef3599
Saul Feliz <646634@gmail.com>
2020-04-15 14:20:51 -0500

    first pass at twoStrings. added algorithm

--- JavaScript/variousProblems/twoStrings.js
+++ JavaScript/variousProblems/twoStrings.js
@@ -13,6 +13,9 @@ function twoStrings(s1, s2) {
   for (let char of s2) {
     s2hash[char] = s2hash[char] + 1 || 1;
   }
+
+  // create a counter. loop through one of the hashes. for every match, increment counter. if at the end of the loop, counter
+  // is at 0, then return No, else yes.
 }
 
 console.log(twoStrings(s1, s2));
