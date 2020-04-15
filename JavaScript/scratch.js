a4b907b2327f184b27059c83e3f94cea426b8183
Saul Feliz <646634@gmail.com>
2020-04-15 13:48:15 -0500

    added twoStrings problem

--- JavaScript/scratch.js
+++ JavaScript/scratch.js
@@ -1,9 +1,15 @@
-const obj = {
-  one: ["1", "uno"],
-  two: 1,
-  three: 2,
-};
+// const obj = {
+//   one: ["1", "uno"],
+//   two: 1,
+//   three: 2,
+// };
 
-// obj.four = "cuatro!";
+// obj.two = "cuatro!";
 
-console.log(obj["two"]);
+console.log([3] == [3]);
+
+// console.log(obj.two);
+
+// const x = 6 % 2;
+
+// console.log(x);
---
+++ JavaScript/variousProblems/twoStrings.js
@@ -0,0 +1,18 @@
+// source: https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+const s1 = "hello";
+const s2 = "world";
+function twoStrings(s1, s2) {
+  let s1hash = {};
+  let s2hash = {};
+
+  for (let char of s1) {
+    s1hash[char] = s1hash[char] + 1 || 1;
+  }
+
+  for (let char of s2) {
+    s2hash[char] = s2hash[char] + 1 || 1;
+  }
+}
+
+console.log(twoStrings(s1, s2));
