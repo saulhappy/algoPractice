803da53835373734524aedd47458045ba50871af
Saul Feliz <646634@gmail.com>
2020-04-18 19:40:03 -0500

    added uniBinaryTree problem

---
+++ JavaScript/trees/uniBinaryTree.js
@@ -0,0 +1,9 @@
+// source : https://leetcode.com/problems/univalued-binary-tree/
+
+// Directions
+// A binary tree is univalued if every node in the tree has the same value.
+// Return true if and only if the given tree is univalued.
+
+function isUnivalTree(root) {}
+
+console.log(isUnivalTree(root));
