4dc5ed65d23abd04c1fc6d16a42ab311499be655
Saul Feliz <646634@gmail.com>
2020-05-29 17:56:42 -0500

    adding LinkedList construction

--- JavaScript/linkedLists/addTwoNumbers.js
+++ JavaScript/linkedLists/addTwoNumbers.js
@@ -1,5 +1,31 @@
 // source: https://leetcode.com/problems/add-two-numbers/
 
-function addTwoNumbers(l1, l2) {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-console.log(addTwoNumbers(l1, l2));
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  printList() {
+    var curr = this.head;
+    var str = "";
+    while (curr) {
+      str += curr.element + " ";
+      curr = curr.next;
+    }
+    console.log(str);
+  }
+}
+
+let l1 = new LinkedList((data = 2));
+
+console.log(l1.printList());
+
+// function addTwoNumbers(l1, l2) {}
+
+// console.log(addTwoNumbers(l1, l2));
