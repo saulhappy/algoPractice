ccfeb33f3bac021135ecd49ba63f2af5528180a5
Saul Feliz <646634@gmail.com>
2020-06-01 04:07:47 -0500

    completed romanToInt problem

--- JavaScript/variousProblems/romanToInt.js
+++ JavaScript/variousProblems/romanToInt.js
@@ -10,14 +10,37 @@ const romanHash = {
   M: 1000,
 };
 
-const s = "II";
+const s = "IX";
+// 1994
 
 function romanToInt(s) {
   let accumulator = 0;
   s = [...s];
 
-  return s;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "I" && s[i + 1] === "V") {
+      accumulator += 4;
+      i++;
+    } else if (s[i] === "I" && s[i + 1] === "X") {
+      accumulator += 9;
+      i++;
+    } else if (s[i] === "X" && s[i + 1] === "L") {
+      accumulator += 40;
+      i++;
+    } else if (s[i] === "X" && s[i + 1] === "C") {
+      accumulator += 90;
+      i++;
+    } else if (s[i] === "C" && s[i + 1] === "D") {
+      accumulator += 400;
+      i++;
+    } else if (s[i] === "C" && s[i + 1] === "M") {
+      accumulator += 900;
+      i++;
+    } else {
+      accumulator += romanHash[s[i]];
+    }
+  }
+  return accumulator;
 }
 
 console.log(romanToInt(s));
-console.log(romanHash);
