fb93b1f8a0b12ff567ed99c303973eed4f1527c7
Saul Feliz <646634@gmail.com>
2020-05-31 20:24:08 -0500

    added hash

--- JavaScript/variousProblems/romanToInt.js
+++ JavaScript/variousProblems/romanToInt.js
@@ -1,7 +1,23 @@
 //source: https://leetcode.com/problems/roman-to-integer/
 
+const romanHash = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
 const s = "II";
 
-function romanToInt(s) {}
+function romanToInt(s) {
+  let accumulator = 0;
+  s = [...s];
+
+  return s;
+}
 
 console.log(romanToInt(s));
+console.log(romanHash);
