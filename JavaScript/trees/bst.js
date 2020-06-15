723cc424896c0f88282fc49945cdc3c758fdf683
Saul Feliz <646634@gmail.com>
2020-06-15 11:05:05 -0500

    added node class

---
+++ JavaScript/trees/bst.js
@@ -0,0 +1,11 @@
+// custom implementation of binary search tree
+
+// 1. create a node class
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
