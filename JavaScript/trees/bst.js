45c171e7f5bb1d8beb8332845d8e84aab7f2de17
Saul Feliz <646634@gmail.com>
2020-06-17 10:16:41 -0500

    completed insert method

--- JavaScript/trees/bst.js
+++ JavaScript/trees/bst.js
@@ -26,6 +26,29 @@ class bst {
     this.count++;
 
     let newNode = new Node(data);
+
+    function searchTree(node) {
+      if (data < node.data) {
+        // if data to be inserted is less than the current node, go left.
+        if (!node.left) {
+          // if there's an empty spot here, append the data to be inserted
+          node.left = newNode;
+        } else {
+          // if there's already a node here, search the left side again with current node ...
+          searchTree(node.left);
+        }
+      } else if (data > node.data)
+        if (!node.right) {
+          // if data to be inserted is greater than the current node, go right.
+          // if there's an empty spot here, append the data to be inserted
+          node.right = newNode;
+        } else {
+          // if there's already a node here, search the right side again with current node ...
+          searchTree(node.right);
+        }
+    }
+    // to initialize searching, call it with the root node...
+    searchTree(this.root);
   }
 
   min() {}
