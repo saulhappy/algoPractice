1d896013fe6ae563d015726147fb6f3331e9b3ae
Saul Feliz <646634@gmail.com>
2020-06-30 12:55:18 -0500

    completed various/jewelsNStones.js problem

--- JavaScript/variousProblems/jewelsNStones.js
+++ JavaScript/variousProblems/jewelsNStones.js
@@ -1,4 +1,4 @@
-source: //leetcode.com/problems/jewels-and-stones/
+// source: https://leetcode.com/problems/jewels-and-stones/
 
 // You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 
@@ -17,9 +17,20 @@ source: //leetcode.com/problems/jewels-and-stones/
 // S and J will consist of letters and have length at most 50.
 // The characters in J are distinct.
 
-https: const J = "aA";
-const S = "aAAbbbb";
+const J = "z";
+const S = "ZZ";
 
-function numJewelsInStones(J, S) {}
+function numJewelsInStones(J, S) {
+  let nJewels = 0;
+
+  for (let i = 0; i < J.length; i++) {
+    for (let j = 0; j < S.length; j++) {
+      if (J[i] === S[j]) {
+        nJewels++;
+      }
+    }
+  }
+  return nJewels;
+}
 
 console.log(numJewelsInStones(J, S));
