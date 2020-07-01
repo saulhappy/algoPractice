58b83ec948c9f87861aed745ebbf6450c2996e6c
Saul Feliz <646634@gmail.com>
2020-07-01 13:20:44 -0500

    added variousProblems/letterComboInPhone.js problem

---
+++ JavaScript/variousProblems/letterComboInPhone.js
@@ -0,0 +1,16 @@
+// source: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+// Example:
+
+// Input: "23"
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+const digits = "23";
+
+function letterCombinations(digits) {}
+
+console.log(letterCombinations(digits));
