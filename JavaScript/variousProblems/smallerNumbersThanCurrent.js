9e340d58669bdd220fb13ba23285e57971e0ea0b
Saul Feliz <646634@gmail.com>
2020-07-03 18:08:32 -0500

    solved variousProblems/smallerNumbersThanCurrent.js problem

--- JavaScript/variousProblems/smallerNumbersThanCurrent.js
+++ JavaScript/variousProblems/smallerNumbersThanCurrent.js
@@ -15,6 +15,19 @@
 
 const nums = [8, 1, 2, 2, 3];
 
-function smallerNumbersThanCurrent(nums) {}
+function smallerNumbersThanCurrent(nums) {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let counter = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (j != i) {
+        if (nums[i] > nums[j]) counter++;
+      }
+    }
+    result.push(counter);
+  }
+  return result;
+}
 
 console.log(smallerNumbersThanCurrent(nums));
