2c59627c707e8e0e58eeff729913ee18300a12ce
Saul Feliz <646634@gmail.com>
2020-07-05 13:44:45 -0500

    added and solved findLongestFlat.js

--- JavaScript/findLongestFlat.js
+++ JavaScript/findLongestFlat.js
@@ -1,11 +1,14 @@
-const arr = [1, 1, 2, 2, 2];
+// source: tripleByte quiz
+// find the longest consecutive count of elements in an array
+// egs: [1, 1, 2] -> 2; [3, 3, 3, 3] -> 4; [2, 2, 2, 3, 4, 3, 3] -> 3
+
+const arr = [1, 1, 2];
 
 function longestFlat(arr) {
   let maxCounter = 0;
-  counter = 1;
+  let counter = 1;
 
   for (let i = 0; i < arr.length; i++) {
-    // debugger;
     if (arr[i] === arr[i + 1]) {
       counter++;
     } else {
--- JavaScript/variousProblems/permuteStrRecursive.js
+++ JavaScript/variousProblems/permuteStrRecursive.js
@@ -1,4 +1,4 @@
-const str = "two";
+const str = "dog";
 
 function findPerms(str) {
   if (str.length === 0) return "";
@@ -9,7 +9,6 @@ function findPerms(str) {
   for (let i = 0; i < str.length; i++) {
     const currentChar = str[i];
     const remainingChars = str.slice(0, i) + str.slice(i + 1);
-    debugger;
 
     for (let permutation of findPerms(remainingChars)) {
       result.push(currentChar + permutation);
