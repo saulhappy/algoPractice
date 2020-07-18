bba93202dce59cde3b3843d5251d536bf13db894
Saul Feliz <646634@gmail.com>
2020-07-17 19:48:48 -0500

    added checkDivNum function to selfDividingNumbers

--- JavaScript/scratch.js
+++ JavaScript/scratch.js
@@ -1,8 +1,1 @@
-const s = "a3112131";
-let sObj = {};
-
-for (let i = 0; i < s.length; i += 2) {
-  sObj[s[i]] = parseInt(s[i + 1]);
-}
-
-console.log(sObj);
+console.log();
--- JavaScript/variousProblems/selfDivNum.js
+++ JavaScript/variousProblems/selfDivNum.js
@@ -4,8 +4,29 @@ const left = 1;
 const right = 22;
 
 function selfDividingNumbers(left, right) {
+  let result = [];
+
   for (let i = left; i <= right; i++) {
-    console.log(i);
+    if (i === 0) return;
+
+    if (i > 0 && i < 10) {
+      result.push(i);
+    } else {
+      checkDivNum(i);
+    }
+
+    function checkDivNum(n) {
+      let ints = [];
+      let str = n.toString();
+
+      for (let char of str) {
+        if (parseInt(str[char]) === 0) {
+          return;
+        } else {
+          ints.push(parseInt(str[char]));
+        }
+      }
+    }
   }
 }
 
