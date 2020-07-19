793dd07448b602465b1482c6a6f49ac22c11ed10
Saul Feliz <646634@gmail.com>
2020-07-18 20:01:18 -0500

    added kthFactorOfN.js problem

---
+++ JavaScript/kthFactorOfN.js
@@ -0,0 +1,20 @@
+source: //leetcode.com/problems/the-kth-factor-of-n/
+
+// Given two positive integers n and k.
+
+// A factor of an integer n is defined as an integer i where n % i == 0.
+
+// Consider a list of all factors of n sorted in ascending order, return the kth factor in this list or return -1 if n has less than k factors.
+
+// Example 1:
+
+// Input: n = 12, k = 3
+// Output: 3
+// Explanation: Factors list is [1, 2, 3, 4, 6, 12], the 3rd factor is 3.
+
+https: const n = 12;
+const k = 3;
+
+function kthFactor(n, k) {}
+
+console.log(kthFactor(n, k));
