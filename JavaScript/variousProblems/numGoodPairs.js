37a99e628dae643bb9e59199b331ce8f238d75e8
Saul Feliz <646634@gmail.com>
2020-07-18 19:43:48 -0500

    solved numGoodPairs.js

--- JavaScript/variousProblems/numGoodPairs.js
+++ JavaScript/variousProblems/numGoodPairs.js
@@ -2,6 +2,18 @@
 
 const nums = [1, 2, 3, 1, 1, 3];
 
-function numIdenticalPairs(nums) {}
+function numIdenticalPairs(nums) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j] && i < j) {
+        count++;
+      }
+    }
+  }
+
+  return count++;
+}
 
 console.log(numIdenticalPairs(nums));
