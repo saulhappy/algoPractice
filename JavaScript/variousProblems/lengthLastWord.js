54b87ad5c39abe90d20697201a0fe137a119e752
Saul Feliz <646634@gmail.com>
2020-07-20 17:42:18 -0500

    solved lengthLastWord.js

---
+++ JavaScript/variousProblems/lengthLastWord.js
@@ -0,0 +1,35 @@
+// source: https://leetcode.com/problems/length-of-last-word/
+
+// Share
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+
+// If the last word does not exist, return 0.
+
+// Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+// Example:
+
+// Input: "Hello World"
+// Output: 5
+
+const s = "lastof ";
+
+function lengthOfLastWord(s) {
+  let newInput = s.trim();
+  let counter = 0;
+  let sArr = [...newInput];
+
+  if (newInput.length === 0) return counter;
+
+  if (!sArr.includes(" ")) return newInput.length;
+
+  for (let i = newInput.length - 1; i > 0; i--) {
+    if (newInput[i] != " ") {
+      counter++;
+    } else {
+      return counter;
+    }
+  }
+}
+
+console.log(lengthOfLastWord(s));
