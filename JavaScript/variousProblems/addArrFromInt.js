53b45b948a47d88eff30ca46593e637193c5d2fc
Saul Feliz <646634@gmail.com>
2020-07-21 17:11:47 -0500

    problem set up

---
+++ JavaScript/variousProblems/addArrFromInt.js
@@ -0,0 +1,18 @@
+// source: https://leetcode.com/problems/add-to-array-form-of-integer/
+
+// For a non-negative integer X, the array-form of X is an array of its digits in left to right order.  For example, if X = 1231, then the array form is [1,2,3,1].
+
+// Given the array-form A of a non-negative integer X, return the array-form of the integer X+K.
+
+// Example 1:
+
+// Input: A = [1,2,0,0], K = 34
+// Output: [1,2,3,4]
+// Explanation: 1200 + 34 = 1234
+
+const A = [1, 2, 0, 0];
+const K = 34;
+
+function addToArrayForm(A, K) {}
+
+addToArrayForm(A, K);
