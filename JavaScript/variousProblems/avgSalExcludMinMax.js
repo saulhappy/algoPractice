b7511df3ccb90971e4ffccfc9b5bc027b24b513c
Saul Feliz <646634@gmail.com>
2020-07-21 16:43:27 -0500

    completed variousProblems/avgSalExcludMinMax.js problem

--- JavaScript/variousProblems/avgSalExcludMinMax.js
+++ JavaScript/variousProblems/avgSalExcludMinMax.js
@@ -1,4 +1,4 @@
-source: //leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+// source: https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
 
 // Given an array of unique integers salary where salary[i] is the salary of the employee i.
 
@@ -17,8 +17,17 @@ source: //leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum
 // Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
 // Average salary excluding minimum and maximum salary is (2000)/1= 2000
 
-https: const salary = [4000, 3000, 1000, 2000];
+const salary = [6000, 5000, 4000, 3000, 2000, 1000];
 
-function average(salary) {}
+function average(salary) {
+  newSal = salary.sort((a, b) => a - b);
+  newSalFiltered = [];
+
+  for (let i = 1; i < newSal.length - 1; i++) {
+    newSalFiltered.push(newSal[i]);
+  }
+
+  return newSalFiltered.reduce((a, b) => a + b, 0) / newSalFiltered.length;
+}
 
 console.log(average(salary));
