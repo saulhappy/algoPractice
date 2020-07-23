63c43742d7afc2cc6120c75df6b9d2ab984a945d
Saul Feliz <646634@gmail.com>
2020-07-23 10:07:04 -0500

    solved variousProblems/bestTimeBuyStock.js

--- JavaScript/scratch.js
+++ JavaScript/scratch.js
@@ -1,15 +1,3 @@
-let array = [1, 2, 3];
+const x = 2;
 
-function test(arr) {
-  let result = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      result.push(`(${i}, ${j})`);
-    }
-  }
-
-  return result;
-}
-
-console.log(test(array).length);
+console.log(-x);
--- JavaScript/variousProblems/bestTimeBuyStock.js
+++ JavaScript/variousProblems/bestTimeBuyStock.js
@@ -2,6 +2,17 @@
 
 const prices = [7, 1, 5, 3, 6, 4];
 
-function maxProfit(prices) {}
+function maxProfit(prices) {
+  let profit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (-prices[i] + prices[j] > profit) {
+        profit = -prices[i] + prices[j];
+      }
+    }
+  }
+  return profit;
+}
 
 console.log(maxProfit(prices));
