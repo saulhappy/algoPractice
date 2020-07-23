8dd9a92348e5d8c4165f8cf9e22fd8d1040fc84c
Saul Feliz <646634@gmail.com>
2020-07-23 14:31:02 -0500

    solved bestTimeBuyStock2.js

--- JavaScript/variousProblems/bestTimeBuyStock2.js
+++ JavaScript/variousProblems/bestTimeBuyStock2.js
@@ -13,8 +13,18 @@
 // Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
 //              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 
-const prices = [7, 1, 5, 3, 6, 4];
+const prices = [7, 6, 4, 3, 1];
 
-function maxProfit(prices) {}
+function maxProfit(prices) {
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i + 1] > prices[i]) {
+      maxProfit += prices[i + 1] - prices[i];
+    }
+  }
+
+  return maxProfit;
+}
 
 console.log(maxProfit(prices));
