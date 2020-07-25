b4235477bb05e43193f23a69b09f8d117cd26704
Saul Feliz <646634@gmail.com>
2020-07-25 13:32:19 -0500

    solved variousProblems/maxConsecOnes.js

--- JavaScript/variousProblems/maxConsecOnes.js
+++ JavaScript/variousProblems/maxConsecOnes.js
@@ -10,6 +10,23 @@
 
 const nums = [1, 1, 0, 1, 1, 1];
 
-function findMaxConsecutiveOnes(nums) {}
+function findMaxConsecutiveOnes(nums) {
+  const isZero = (currentValue) => currentValue === 0;
+
+  if (nums.every(isZero)) return 0;
+  else {
+    let max = 0;
+    let counter = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 1 && nums[i + 1] === 1) counter++;
+      else {
+        if (max < counter) max = counter;
+        counter = 1;
+      }
+    }
+    return max;
+  }
+}
 
 console.log(findMaxConsecutiveOnes(nums));
