ef1b8ed340563cae43ed811fbf08c42dc4d1e44f
Saul Feliz <646634@gmail.com>
2020-07-25 12:00:36 -0500

    problem set up

---
+++ JavaScript/variousProblems/consecChars.js
@@ -0,0 +1,20 @@
+// source: https://leetcode.com/problems/consecutive-characters/
+
+// Given a string s, the power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+// Return the power of the string.
+
+// Example 1:
+
+// Input: s = "leetcode"
+// Output: 2
+// Explanation: The substring "ee" is of length 2 with the character 'e' only.
+// Example 2:
+
+// Input: s = "abbcccddddeeeeedcba"
+// Output: 5
+// Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+
+function maxPower(s) {}
+
+console.log(maxPower(s));
