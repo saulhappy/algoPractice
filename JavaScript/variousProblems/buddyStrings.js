51eb1b96c67607473c7e1996c15e8c7e525fd1d4
Saul Feliz <646634@gmail.com>
2020-07-26 10:45:46 -0500

    set up Buddy Strings problem

---
+++ JavaScript/variousProblems/buddyStrings.js
@@ -0,0 +1,15 @@
+// source: https://leetcode.com/problems/buddy-strings/
+
+// Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
+
+// Example 1:
+
+// Input: A = "ab", B = "ba"
+// Output: true
+
+const A = "ab";
+const B = "ba";
+
+function buddyStrings(A, B) {}
+
+console.log(buddyStrings(A, B));
