6e2fdaeca7cf6c0fbec2bf22d7d89179ba3af0c9
Saul Feliz <646634@gmail.com>
2020-07-30 17:47:02 -0500

    practiced python string operators

---
+++ python/masterclass/stringOperators.py
@@ -0,0 +1,22 @@
+print("hello " * 5)
+print("")
+
+
+print("fri" in "friday")
+print("day" in "friday")
+print("")
+
+age = 40
+when = "today"
+print("I am " + str(age) + " years old " + when)
+
+# or, can use replacement fields
+
+print("I am {0} years old {1}".format(age, when))
+
+# or, can use f strings
+print(f"I am {age} years old {when}")
+
+
+print("")
+print("")
