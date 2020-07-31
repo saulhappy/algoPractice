5893bcb88d2102f7fd34f69f322438079dbede15
Saul Feliz <646634@gmail.com>
2020-07-31 11:17:43 -0500

    updated blocks.py

--- python/masterclass/blocks.py
+++ python/masterclass/blocks.py
@@ -4,6 +4,16 @@ print("")
 print(f"{name} is {age} years old ")
 
 if age < 18:
-    print(f"You're not old enought to vote yet, {name} ")
+    print(f"{name}, please try to vote in {18 - age} year(s) ")
 else:
-    print(f"You can vote {name} ")
+    print(f"You can vote, {name}")
+
+x = 5
+y = 7
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is smaller than y")
+else:
+    print("x equals y")
