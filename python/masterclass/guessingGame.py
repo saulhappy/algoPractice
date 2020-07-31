c75c06aed9e25717d36a47680a74c3a75ab176f3
Saul Feliz <646634@gmail.com>
2020-07-31 11:25:52 -0500

    updated guessing game

--- python/masterclass/guessingGame.py
+++ python/masterclass/guessingGame.py
@@ -2,3 +2,16 @@ answer = 3
 
 print("Guess a number between 1 and 10: ")
 guess = int(input())
+
+if guess < answer:
+    print("Guess a little higher ")
+    guess = int(input())
+    if guess == answer:
+        print("You got it!")
+elif guess > answer:
+    print("Guess a little lower ")
+    guess = int(input())
+    if guess == answer:
+        print("You got it!")
+else:
+    print("You nailed it!")
