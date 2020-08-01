14792e4d21963ea6caf9980f8e42a35b14e01b34
Saul Feliz <646634@gmail.com>
2020-08-01 16:57:19 -0500

    did fizzbuzz in python

--- python/masterclass/forLoops.py
+++ python/masterclass/forLoops.py
@@ -1,8 +1,11 @@
-balance = "9,564,564,323.23"
-seperators = ""
+# balance = "9,564,564,323.23"
+# seperators = ""
 
-for char in balance:
-    if not char.isdigit():
-        seperators += char
+# for char in balance:
+#     if not char.isdigit():
+#         seperators += char
 
-print(seperators)
+# print(seperators)
+
+for i in range(1, 6):
+    print(f"the number is now {i}")
