97264fe7b0f49b8ebe77abdcb38451dc90cbed15
Saul Feliz <646634@gmail.com>
2020-07-31 23:35:49 -0500

    refactored guessingGame.py

--- python/masterclass/guessingGame.py
+++ python/masterclass/guessingGame.py
@@ -3,15 +3,34 @@ answer = 3
 print("Guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Guess a little higher ")
-    guess = int(input())
-    if guess == answer:
-        print("You got it!")
-elif guess > answer:
-    print("Guess a little lower ")
-    guess = int(input())
-    if guess == answer:
-        print("You got it!")
+# if guess < answer:
+#     print("Guess a little higher ")
+#     guess = int(input())
+#     if guess == answer:
+#         print("You got it!")
+#     else:
+#         print("Wrong again")
+# elif guess > answer:
+#     print("Guess a little lower ")
+#     guess = int(input())
+#     if guess == answer:
+#         print("You got it!")
+#     else:
+#         print("Wrong again")
+# else:
+#     print("You nailed it right away!"
+
+# REFACTOR
+
+if guess == answer:
+    print("You nailed it right away!")
 else:
-    print("You nailed it!")
+    if guess < answer:
+        print("Guess a little higher")
+    else:
+        print("Guess a bit lower")
+    guess = int(input())
+    if guess == answer:
+        print("you got it!")
+    else:
+        print("sorry, you didn't get it")
