a30b0656bd851122ec52a36b5a40d36a83527735
Saul Feliz <646634@gmail.com>
2020-08-02 12:20:23 -0500

    added directiond demo

--- python/masterclass/whileLoops.py
+++ python/masterclass/whileLoops.py
@@ -1,7 +1,16 @@
 # for i in range(6):
 #     print(f"i now {i}")
 
-i = 0
-while i < 6:
-    print(f"i is now {i}")
-    i += 1
+# i = 0
+# while i < 6:
+#     print(f"i is now {i}")
+#     i += 1
+
+exits = ["north", "east"]
+
+choice = ""
+
+while choice not in exits:
+    choice = input("pick a direction: ")
+
+print("you're out!")
