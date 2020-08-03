842ba03864327c5134365f5bf82402107adacaf7
Saul Feliz <646634@gmail.com>
2020-08-03 10:52:41 -0500

    updated lemonadeChage.js

--- JavaScript/variousProblems/lemonadeChange.js
+++ JavaScript/variousProblems/lemonadeChange.js
@@ -2,6 +2,27 @@
 
 const bills = [5, 5, 5, 10, 20];
 
-function lemonadeChange(bills) {}
+function lemonadeChange(bills) {
+  let billsObj = {};
+  let changeDue = 0;
+
+  for (let bill of bills) {
+    billsObj[bill] = billsObj[bill] + 1 || 1;
+    changeDue = bill - 5;
+    if (changeDue > 0) {
+      let cashOnHand = 0;
+      let changeGiven = 0;
+
+      for (let bill in billsObj) {
+        cashOnHand += billsObj[bill] * bill;
+        if (cashOnHand < changeDue) return false;
+
+        debugger;
+      }
+    }
+  }
+
+  return false;
+}
 
 console.log(lemonadeChange(bills));
