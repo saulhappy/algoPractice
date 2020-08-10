999f6ad93cfdab87b9deafb58ae12930beea8c7d
Saul Feliz <646634@gmail.com>
2020-08-09 20:06:29 -0500

    kthMissingPstvNmbr problem setup

---
+++ JavaScript/variousProblems/kthMissingPstvNmbr.js
@@ -0,0 +1,15 @@
+// source: https://leetcode.com/problems/kth-missing-positive-number/
+
+// Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+// Find the kth positive integer that is missing from this array.
+
+// Input: arr = [2,3,4,7,11], k = 5
+// Output: 9
+// Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+const arr = [2, 3, 4, 7, 11];
+const k = 5;
+// 9
+
+function findKthPositive(arr, k) {}
