f5db243aa573a2f94046676757c004e82f899ac4
Saul Feliz <646634@gmail.com>
2020-08-12 10:19:37 -0500

    completed minStepsToOneRecursion with simple non-memoized function

---
+++ JavaScript/variousProblems/minStepsToOneII.js
@@ -0,0 +1,23 @@
+// simple recursion
+
+const n = 10;
+
+function minStepsToOneRecursion(n) {
+  if (n === 1) {
+    return 0;
+  }
+
+  let result = minStepsToOneRecursion(n - 1);
+
+  if (n % 2 === 0) {
+    result = Math.min(result, minStepsToOneRecursion(n / 2));
+  }
+
+  if (n % 3 === 0) {
+    result = Math.min(result, minStepsToOneRecursion(n / 3));
+  }
+
+  return result + 1;
+}
+
+console.log(minStepsToOneRecursion(n));
