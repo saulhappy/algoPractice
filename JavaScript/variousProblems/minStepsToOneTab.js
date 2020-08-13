e095002722cea5b93b8820f3995c5594cec95fc9
Saul Feliz <646634@gmail.com>
2020-08-13 15:09:06 -0500

    first pass at minStepsToOneTab.js

--- JavaScript/variousProblems/minStepsToOneTab.js
+++ JavaScript/variousProblems/minStepsToOneTab.js
@@ -1,5 +1,21 @@
 // TABULATED SOLUTION
 
-function minStepsToOneTab(n) {}
+function minStepsToOneTab(n) {
+  let table = [-1, 0];
 
-console.log(minStepsToOneTab(3));
+  for (let i = 2; i < n; i++) {
+    table[i] = Math.min(table[i + 1], table[i] + 1);
+  }
+
+  if (i * 2 <= n) {
+    table[i] = Math.min(table[i + 1], table[i * 2]);
+  }
+
+  if (i * 3 <= n) {
+    table[i * 3] = Math.min(table[i + 1], table[i * 3]);
+  }
+
+  return table[n];
+}
+
+console.log(minStepsToOneTab(10));
