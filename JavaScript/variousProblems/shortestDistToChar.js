0fd1e55ca617e19965567b265b4b65b589707f56
Saul Feliz <646634@gmail.com>
2020-08-16 10:03:48 -0500

    added shortestDistToChar.js problem

---
+++ JavaScript/variousProblems/shortestDistToChar.js
@@ -0,0 +1,15 @@
+// source: https://leetcode.com/problems/shortest-distance-to-a-character/
+
+// Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+
+// Example 1:
+
+// Input: S = "loveleetcode", C = 'e'
+// Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+
+const S = "loveleetcode";
+const C = "e";
+
+function shortestToChar(S, C) {}
+
+console.log(shortestToChar(S, C));
