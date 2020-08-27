e2755fa08c0530498ef2bf525181136c886bec20
Saul Feliz <646634@gmail.com>
2020-08-26 22:34:38 -0500

    added nested_extraction.py

---
+++ python/miscPracticeProblems/nested_extraction.py
@@ -0,0 +1,12 @@
+# Declare a nested_extraction function that accepts a list of lists and an index position.
+#
+# The function should use the index as the basis of finding both the nested list
+# and the element from that list with the given index position
+#
+# You can assume the number of lists will always be equal to
+# the number of elements within each of them.
+#
+# nl = [[3, 4, 5], [7, 8, 9], [10, 11, 12]]
+# nested_extraction(nl, 0)  => 3
+# nested_extraction(nl, 1)  => 8
+# nested_extraction(nl, 2)  => 12
