d64b4d45c29a984ac9c02698860b8e8ba82be0c5
Saul Feliz <646634@gmail.com>
2020-08-28 12:29:02 -0500

    completed nested_extraction.py

--- python/miscPracticeProblems/nested_extraction.py
+++ python/miscPracticeProblems/nested_extraction.py
@@ -6,7 +6,14 @@
 # You can assume the number of lists will always be equal to
 # the number of elements within each of them.
 #
-# nl = [[3, 4, 5], [7, 8, 9], [10, 11, 12]]
+nl = [[3, 4, 5], [7, 8, 9], [10, 11, 12]]
 # nested_extraction(nl, 0)  => 3
 # nested_extraction(nl, 1)  => 8
 # nested_extraction(nl, 2)  => 12
+
+
+def nested_extraction(nl, num):
+    return nl[num][num]
+
+
+print(nested_extraction(nl, 2))
