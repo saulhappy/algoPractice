0ff069bf543bc35bbcd9bd686b22d2bec127b2ee
Saul Feliz <646634@gmail.com>
2020-08-28 13:38:53 -0500

    solved long_word_in_collection.py

---
+++ python/miscPracticeProblems/longWordInCol.py
@@ -0,0 +1,16 @@
+# Declare a long_word_in_collection function that accepts a list and a string.
+# The function should return True if
+#   - the string exists in the list AND
+#   - the string has more than 4 characters.
+#
+words = ["cat", "dog", "rhino"]
+# long_word_in_collection(words, "rhino")  => True
+# long_word_in_collection(words, "cat")    => False
+# long_word_in_collection(words, "monkey") => False
+
+
+def long_word_in_collection(list, string):
+    return string in words and len(string) > 4
+
+
+print(long_word_in_collection(list, "cat"))
