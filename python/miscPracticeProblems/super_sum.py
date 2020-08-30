34aacd0fb46bf20ffd203cdd3be3637933e7e768
Saul Feliz <646634@gmail.com>
2020-08-30 16:37:15 -0500

    other solution for super_sum

--- python/miscPracticeProblems/super_sum.py
+++ python/miscPracticeProblems/super_sum.py
@@ -1,10 +1,19 @@
+# def super_sum(strings):
+#     sum_indices = 0
+#     for word in strings:
+#         for i in range(len(word)):
+#             if word[i] == "s":
+#                 sum_indices += i
+#                 break
+#     return sum_indices
+
+
 def super_sum(strings):
-    sum_indices = 0
+    result = 0
     for word in strings:
-        for i in range(len(word)):
-            if word[i] == "s":
-                sum_indices += i
-    return sum_indices
+        if "s" in word:
+            result += word.find("s")
+    return result
 
 
-print(super_sum(["mustache"]))
+print(super_sum(["mustache", "pessimist"]))
