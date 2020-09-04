64b2dd5e31d9e3cf537edc160d429b4475cb51c0
Saul Feliz <646634@gmail.com>
2020-09-04 13:01:24 -0500

    solved del_all.py

---
+++ python/miscPracticeProblems/del_all.py
@@ -0,0 +1,21 @@
+# Declare a delete_all function that accepts a list of strings and a target string
+# Remove all occurrences of the target string from the list and return it
+#
+# EXAMPLES
+# delete_all([1, 3, 5], 3)  => [1, 5]
+# delete_all([5, 3, 5], 5)  => [3]
+# delete_all([4, 4, 4], 4)  => []
+# delete_all([4, 4, 4], 6)  => [4, 4, 4]
+
+heroes = ["flash", "flash"]
+
+
+def delete_all(lst, target):
+    while target in lst:
+        lst.remove(target)
+    return lst
+
+
+delete_all(heroes, "flash")
+
+print(heroes)
