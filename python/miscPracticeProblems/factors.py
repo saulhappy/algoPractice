8ad18d247f1ce1cf76ece8c32bde975f46c4c43f
Saul Feliz <646634@gmail.com>
2020-09-03 21:44:41 -0500

    solved factors.py

---
+++ python/miscPracticeProblems/factors.py
@@ -0,0 +1,19 @@
+# Write a factors function that accepts a positive whole number
+# It should return a list of all of the number's factors in ascending order
+# HINT: Could the range function be helpful here? Or maybe a while loop?
+#
+# EXAMPLES
+# factors(1)  => [1]
+# factors(2)  => [1, 2]
+# factors(10) => [1, 2, 5, 10]
+# factors(64) => [1, 2, 4, 8, 16, 32, 64]
+
+def factors(number):
+    result = []
+    for num in range(1, number + 1):
+        if number % num == 0:
+            result.append(num)
+    return result
+
+
+print(factors(64))
