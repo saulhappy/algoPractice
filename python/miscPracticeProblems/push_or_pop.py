7b8ba5e63be7e640f20466fd18f05afa654f52a7
Saul Feliz <646634@gmail.com>
2020-09-05 18:18:01 -0500

    solved push_or_pop.py

---
+++ python/miscPracticeProblems/push_or_pop.py
@@ -0,0 +1,27 @@
+# Declare a push_or_pop function that accepts a list of numbers
+# Build up and return a new list by iterating over the list of numbers
+# If a number is greater than 5, add it to the end of the new list
+# If a number is less than or equal to 5, remove the last element added to the new list
+# Assume the order of numbers in the argument will never require removing from an empty list
+#
+# EXAMPLES
+# push_or_pop([10])            => [10]
+# push_or_pop([10, 4])         => []
+# push_or_pop([10, 20, 30])    => [10, 20, 30]
+# push_or_pop([10, 20, 2, 30]) => [10, 30]
+
+numbers = [10, 20, 2, 30]
+
+
+def push_or_pop(numbers):
+    result = []
+
+    for num in numbers:
+        if num > 5:
+            result.append(num)
+        else:
+            result.pop()
+    return result
+
+
+print(push_or_pop(numbers))
