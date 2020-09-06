c9c101fbe8e370524b8c95a05bde5cdf799e321c
Saul Feliz <646634@gmail.com>
2020-09-06 13:06:59 -0500

    refactor encrypt

--- python/miscPracticeProblems/encrypt.py
+++ python/miscPracticeProblems/encrypt.py
@@ -8,20 +8,31 @@
 # encrypt_message("xyz") => "zab"
 # encrypt_message("")    => ""
 
-string = "abc"
+string = ""
 
+# saul solution
+# def encrypt_message(string):
+#     result = ""
+#     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+#                 "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+#     for char in string:
+#         if char == "y":
+#             result += alphabet[0]
+#         elif char == "z":
+#             result += alphabet[1]
+#         else:
+#             result += alphabet[alphabet.index(char) + 2]
+#     return result
+
+
+# alternative
 
 def encrypt_message(string):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     result = ""
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-                "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     for char in string:
-        if char == "y":
-            result += alphabet[0]
-        elif char == "z":
-            result += alphabet[1]
-        else:
-            result += alphabet[alphabet.index(char) + 2]
+        index_position = (alphabet.index(char) + 2) % 26
+        result += alphabet[index_position]
     return result
 
 
