b989baa83f15b57c2f5f7e76c1e3f8f28f64d678
Saul Feliz <646634@gmail.com>
2020-09-09 23:16:06 -0500

    invert.py setup

---
+++ python/miscPracticeProblems/Dictionaries/invert.py
@@ -0,0 +1,20 @@
+# Declare an invert function that accepts a dictionary object.
+# The function should return a new dictionary where the keys and values from the original dictionary are inverted.
+# Each key should now be a value, and each value should be a key.
+# Assume both the keys and values of the dictionary are immutable.
+#
+# EXAMPLE:
+my_dict = {
+    "A": "B",
+    "C": "D",
+    "E": "F"
+}
+#
+# invert(my_dict) => {'B': 'A', 'D': 'C', 'F': 'E'}
+
+
+def invert(my_dict):
+    return my_dict
+
+
+print(invert(my_dict))
