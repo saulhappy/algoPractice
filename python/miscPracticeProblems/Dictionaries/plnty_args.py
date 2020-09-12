a1c189fcc21d888130b14fbdf09d8e375de3cae7
Saul Feliz <646634@gmail.com>
2020-09-12 00:03:59 -0500

    set up plnty_args.py problem

---
+++ python/miscPracticeProblems/Dictionaries/plnty_args.py
@@ -0,0 +1,19 @@
+# Declare a plenty_of_arguments function that accepts two parameters (a and b)
+# and an additional **kwargs parameter.
+#
+# The function should return True if the sum of a, b, and the values of
+# **kwargs is greater than 100. It should return False otherwise.
+#
+# EXAMPLES:
+# plenty_of_arguments(20, 30)                          => False
+# plenty_of_arguments(a = 50, b = 75)                  => True
+# plenty_of_arguments(a = 50, b = 25, c = 50)          => True
+# plenty_of_arguments(a = 25, b = 25, c = 25, d = 25)  => False
+# plenty_of_arguments(a = 25, b = 25, c = 25, d = 26)  => True
+
+
+def plenty_of_arguments():
+    return
+
+
+print(plenty_of_arguments())
