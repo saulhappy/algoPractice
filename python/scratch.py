f198e1d2ce9b53fec66c83dbbaaba790125ac9a4
Saul Feliz <646634@gmail.com>
2020-09-15 16:00:04 -0500

    added algorithms/majority_element.py

--- python/scratch.py
+++ python/scratch.py
@@ -1,2 +1,12 @@
-values = [1, 2, 3, 4, 5]
-print({value: sum(values[:index + 2]) for index, value in enumerate(values)})
+movies = set(["Godfather", "Shawshank Redemption", "The Matrix"])
+print(movies)
+
+movies.add("Star Wars")
+print(movies)
+
+movies.update(["Star Wars", "Top Gun"])
+movies.add(2)
+print(movies)
+
+
+print(set("zyx"))
