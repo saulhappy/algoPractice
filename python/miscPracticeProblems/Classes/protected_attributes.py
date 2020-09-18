28a911ae6f2195ef323a53312b7d2cdda4f3d25f
Saul Feliz <646634@gmail.com>
2020-09-17 19:46:45 -0500

    solved classes/protected_attributes.py

--- python/miscPracticeProblems/Classes/protected_attributes.py
+++ python/miscPracticeProblems/Classes/protected_attributes.py
@@ -26,6 +26,22 @@
 
 # See sample execution below
 
+class Book():
+    def __init__(self, author, publisher, page_count):
+        self._author = author
+        self._publisher = publisher
+        self.page_count = page_count
+
+    def copyright(self):
+        return f"Copyright {self._author}, {self._publisher}"
+
+    def rip_in_half(self):
+        if self.page_count > 1:
+            self.page_count /= 2
+        else:
+            self.page_count = 0
+
+
 book = Book(author="Grant Cardone", publisher="10X Enterprises", page_count=10)
 
 print(book.copyright())  # Copyright Grant Cardone, 10X Enterprises
