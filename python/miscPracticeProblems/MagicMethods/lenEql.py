290edede2295e4afbc07080b1d57445360632a87
Saul Feliz <646634@gmail.com>
2020-09-19 15:50:35 -0500

    solved MagicMethods/lenEql.py

--- python/miscPracticeProblems/MagicMethods/lenEql.py
+++ python/miscPracticeProblems/MagicMethods/lenEql.py
@@ -14,32 +14,47 @@
 # Make it so two newspapers are considered equal if they have the
 # same number of pages AND the same number of sections
 
+class Newspaper():
+    def __init__(self, pages, sections):
+        self.pages = pages
+        self.sections = sections
+
+    def __len__(self):
+        return self.pages
+
+    def __getitem__(self, key):
+        return self.sections[key]
+
+    def __eq__(self, other_obj):
+        return self.pages == other_obj.pages and len(self.sections) == len(other_obj.sections)
+
+
 # EXAMPLE:
 
-# monday_sections = {
-#   "Politics": "A5",
-#   "Sports": "B2",
-#   "Entertainment": "C3"
-# }
+monday_sections = {
+    "Politics": "A5",
+    "Sports": "B2",
+    "Entertainment": "C3"
+}
 
-# tuesday_sections = {
-#   "Travel": "A5",
-#   "Cooking": "B2",
-# }
+tuesday_sections = {
+    "Travel": "A5",
+    "Cooking": "B2",
+}
 
-# wednesday_sections = {
-#   "Classifieds": "A5",
-#   "Weddings": "B2",
-#   "Weather": "C3"
-# }
+wednesday_sections = {
+    "Classifieds": "A5",
+    "Weddings": "B2",
+    "Weather": "C3"
+}
 
-# np1 = Newspaper(pages = 80, sections = monday_sections)
-# np2 = Newspaper(pages = 60, sections = tuesday_sections)
-# np3 = Newspaper(pages = 80, sections = wednesday_sections)
+np1 = Newspaper(pages=80, sections=monday_sections)
+np2 = Newspaper(pages=60, sections=tuesday_sections)
+np3 = Newspaper(pages=80, sections=wednesday_sections)
 
-# print(len(np1))        # 80
-# print(len(np2))        # 60
-# print(np1 == np2)      # False -- np1 has 3 sections while np2 has 2 sections
-# print(np1 == np3)      # True -- both have 80 pages and 3 sections
-# print(np1["Politics"]) # "A5"
-# print(np2["Cooking"])  # "B2"
+print(len(np1))        # 80
+print(len(np2))        # 60
+print(np1 == np2)      # False -- np1 has 3 sections while np2 has 2 sections
+print(np1 == np3)      # True -- both have 80 pages and 3 sections
+print(np1["Politics"])  # "A5"
+print(np2["Cooking"])  # "B2"
