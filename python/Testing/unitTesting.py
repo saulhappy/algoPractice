a630e23669fa25b3c30ceaeb7005f5c1bedf42c9
Saul Feliz <646634@gmail.com>
2020-09-21 11:54:53 -0500

    added multiply method and test

--- python/Testing/unitTesting.py
+++ python/Testing/unitTesting.py
@@ -1,11 +1,15 @@
 import unittest
 """
-Importing unittest, and creating a class that inherits TestCast from it. 
+Importing unittest, and creating a class that inherits TestCast from it.
 Using assertEqual to test Python's split and count functions.
-Each instance method must contain "test" in name, conventionally, at the beginning. 
-Each . is a resulting test. 
-assertEqual takes 2 arguments: the test, and expected result. 
+Each instance method must contain "test" in name, conventionally, at the beginning.
+Each . is a resulting test.
+assertEqual takes 2 arguments: the test, and expected result.
 """
+
+
+def multiply(a, b):
+    return a * b
 
 
 class TestStringMethods(unittest.TestCase):
@@ -15,6 +19,11 @@ class TestStringMethods(unittest.TestCase):
 
     def test_count(self):
         self.assertEqual("beautiful".count("u"), 2)
+
+
+class TestMultiply(unittest.TestCase):
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 7), 21)
 
 
 if __name__ == "__main__":
