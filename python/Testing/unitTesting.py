c6075a05c42c590bf897045d4b44ddca0c8f9beb
Saul Feliz <646634@gmail.com>
2020-09-21 12:22:08 -0500

    added assertNot Equal, custom message to Testing/unitTesting.py

--- python/Testing/unitTesting.py
+++ python/Testing/unitTesting.py
@@ -1,15 +1,24 @@
 import unittest
 """
-Importing unittest, and creating a class that inherits TestCast from it.
-Using assertEqual to test Python's split and count functions.
+Importing unittest, and creating a classes that inherits TestCast from it.
+Using various methods from TestCast to test Python internal methods, and custom functions.
 Each instance method must contain "test" in name, conventionally, at the beginning.
 Each . is a resulting test.
 assertEqual takes 2 arguments: the test, and expected result.
+The last argument of an assertion is a custom message for when a test fails.
 """
+
+# in practice, these functions should be imported from other module.
 
 
 def multiply(a, b):
     return a * b
+
+
+def copy_and_add_element(values, element):
+    copy = values[:]
+    copy.append(element)
+    return copy
 
 
 class TestStringMethods(unittest.TestCase):
@@ -30,6 +39,24 @@ class TestSwapCase(unittest.TestCase):
     @unittest.skip("Testing skip method")
     def test_swap_case(self):
         pass
+
+
+class TestInequality(unittest.TestCase):
+    def test_inequality(self):
+        self.assertNotEqual(1, 2)
+        self.assertNotEqual([1, 2], [2, 1])
+
+    def test_copy_and_add_element(self):
+        values = [1, 2, 3]
+        result = copy_and_add_element(values, 4)
+
+        self.assertEqual(result, [1, 2, 3, 4])
+
+        self.assertNotEqual(
+            values,
+            [1, 2, 3, 4],
+            "The copy_and_add_element function is mutating the input. Make sure you're creating a copy."
+        )
 
 
 if __name__ == "__main__":
