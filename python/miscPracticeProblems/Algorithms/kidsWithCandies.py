f528d9e478cab4d68cf72891a37b643f0ffd13f9
Saul Feliz <646634@gmail.com>
2020-09-25 05:26:50 -0500

    added kidsWithCandies.py

---
+++ python/miscPracticeProblems/Algorithms/kidsWithCandies.py
@@ -0,0 +1,25 @@
+"""
+source: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+Given the array candies and the integer extraCandies, where candies[i] represents the number 
+of candies that the ith kid has.
+
+For each kid check if there is a way to distribute extraCandies among the kids 
+such that he or she can have the greatest number of candies among them. 
+Notice that multiple kids can have the greatest number of candies.
+
+Example 1:
+
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+
+"""
+
+candies = [2, 3, 5, 1, 3]
+extraCandies = 3
+
+
+def kidsWithCandies(e_c):
+    pass
+
+
+print(kidsWithCandies(extraCandies))
