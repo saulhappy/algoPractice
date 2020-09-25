d47a1eb3ce95a23db4d9757de2ea06b6b7069522
Saul Feliz <646634@gmail.com>
2020-09-25 18:52:49 -0500

    solved algorithms/kidsWithCandles.py

--- python/miscPracticeProblems/Algorithms/kidsWithCandies.py
+++ python/miscPracticeProblems/Algorithms/kidsWithCandies.py
@@ -18,8 +18,14 @@ candies = [2, 3, 5, 1, 3]
 extraCandies = 3
 
 
-def kidsWithCandies(e_c):
-    pass
+def kidsWithCandies(candies, extraCandies):
+    result = []
+    for i in range(len(candies)):
+        if candies[i] + extraCandies >= max(candies):
+            result.append(True)
+        else:
+            result.append(False)
+    return result
 
 
-print(kidsWithCandies(extraCandies))
+print(kidsWithCandies(candies, extraCandies))
