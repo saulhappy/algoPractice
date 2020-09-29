6b7b608c12bb1476dfe41601437ece06c4222cb9
Saul Feliz <646634@gmail.com>
2020-09-29 10:36:49 -0500

    added algorithms/jewels_and_stones.py

--- python/miscPracticeProblems/Algorithms/good_pairs.py
+++ python/miscPracticeProblems/Algorithms/good_pairs.py
@@ -1,4 +1,5 @@
 """
+source: https://leetcode.com/problems/number-of-good-pairs/
 Given an array of integers nums.
 A pair (i,j) is called good if nums[i] == nums[j] and i < j.
 Return the number of good pairs.
