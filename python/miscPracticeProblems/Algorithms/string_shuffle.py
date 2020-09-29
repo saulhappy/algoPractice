8b63a5a5b216acd5ab8c8562fe05d3a618d92a62
Saul Feliz <646634@gmail.com>
2020-09-29 13:31:21 -0500

    added algorithms/string_shuffle.py

---
+++ python/miscPracticeProblems/Algorithms/string_shuffle.py
@@ -0,0 +1,23 @@
+"""
+source: https://leetcode.com/problems/shuffle-string/
+
+Given a string s and an integer array indices of the same length.
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+Return the shuffled string.
+
+Example 1:
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+"""
+
+s = "codeleet"
+indices = [4, 5, 6, 7, 0, 2, 1, 3]
+
+
+def restoreString(s, indeces):
+    pass
+
+
+print(restoreString(s, indices))
