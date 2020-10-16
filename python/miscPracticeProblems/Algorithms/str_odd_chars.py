a8bea34f618278025048d802c3b1da6dfe68f998
Saul Feliz <646634@gmail.com>
2020-10-15 23:04:04 -0500

    added str_odd_chars.py

---
+++ python/miscPracticeProblems/Algorithms/str_odd_chars.py
@@ -0,0 +1,10 @@
+"""
+source: https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+
+"""
+
+n = 4
+
+
+def generateTheString(n):
+    pass
--- python/scratch.py
+++ python/scratch.py
@@ -1,4 +1,17 @@
-n = 10
+admins = [
+    ("""Scott White""", "scott.white@huvrdata.com"),
+    ("""Jeff Sheffield""", "jeff.sheffield@huvrdata.com"),
+    ("""Nick Fang""", "nick.fang@huvrdata.com"),
+    ("""Julia Guimiot""", "julia.guimiot@huvrdata.com"),
+    ("""Sean Hong""", "sean.hong@huvrdata.com"),
+    ("""Saul Feliz""", "saul.feliz@huvrdata.com"),
+    ("""Steve Hatch""", "steve.hatch@huvrdata.com"),
+    ("""William Wigfield""", "william.wigfield@huvrdata.com"),
+]
 
-for i in range(1, n):
-    print(i)
+# admin_emails = []
+# for admin in admins:
+#     admin_emails.append(admin[1])
+
+admin_emails = [admin[1] for admin in admins]
+print(admin_emails)
