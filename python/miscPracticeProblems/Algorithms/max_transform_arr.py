992dde62fd042c4781d552bf56a0ab958a9ca5b0
Saul Feliz <646634@gmail.com>
2020-10-25 22:45:52 -0500

    first pass max_transform_arr.py

--- python/miscPracticeProblems/Algorithms/max_transform_arr.py
+++ python/miscPracticeProblems/Algorithms/max_transform_arr.py
@@ -3,8 +3,26 @@ source: https://leetcode.com/problems/rank-transform-of-an-array/
 
 """
 
-arr = [40, 10, 20, 30]
+arr = [37, 12, 28, 9, 100, 56, 80, 5, 12, 12]
+#     [5,  3,  4,  2, 8,   6,  7,  1, 3,  3]
 
 
 def arrayRankTransform(arr):
-    pass
+    arrCopy = arr[:]
+    arrCopy.sort()
+    arrSet = list(set(arr))
+    arrRank = []
+    seen = []
+
+    for i in range(len(arr)):
+        for j in range(len(arrCopy)):
+            if arr[i] in seen:
+                arrRank.append(j + 1)
+            if arr[i] == arrCopy[j]:
+                arrRank.append(j + 1)
+                seen.append(arr[i])
+
+    return arrRank
+
+
+print(arrayRankTransform(arr))
