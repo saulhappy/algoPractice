7283b115d4f1a0b34f5a67019ca03f4a0ff2fe3b
Saul Feliz <646634@gmail.com>
2020-11-08 16:06:18 -0600

    solved str)od_chars.py

--- python/miscPracticeProblems/Algorithms/str_odd_chars.py
+++ python/miscPracticeProblems/Algorithms/str_odd_chars.py
@@ -3,8 +3,14 @@ source: https://leetcode.com/problems/generate-a-string-with-characters-that-hav
 
 """
 
-n = 4
+n = 5
 
 
 def generateTheString(n):
-    pass
+    if n % 2 == 0:
+        return "a" * (n - 1) + "b"
+    else:
+        return "a" * n
+
+
+print(generateTheString(n))
