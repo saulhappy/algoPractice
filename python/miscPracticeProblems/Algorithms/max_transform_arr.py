af06eeee03470b6d9aff14ec2965a7857aa92273
Saul Feliz <646634@gmail.com>
2020-11-12 14:36:28 -0600

    updated max_transform_arr.py algorithm

--- python/miscPracticeProblems/Algorithms/max_transform_arr.py
+++ python/miscPracticeProblems/Algorithms/max_transform_arr.py
@@ -6,23 +6,14 @@ source: https://leetcode.com/problems/rank-transform-of-an-array/
 arr = [37, 12, 28, 9, 100, 56, 80, 5, 12, 12]
 #     [5,  3,  4,  2, 8,   6,  7,  1, 3,  3]
 
+"""
+Try this algorithm:
+1. Create a non-dupe set it. Sort. 
+2. For each element of set, make object with element, index + 1 as value.
+3. Look up each element of input arr, append return value to result arr. 
+"""
 
 def arrayRankTransform(arr):
-    arrCopy = arr[:]
-    arrCopy.sort()
-    arrSet = list(set(arr))
-    arrRank = []
-    seen = []
-
-    for i in range(len(arr)):
-        for j in range(len(arrCopy)):
-            if arr[i] in seen:
-                arrRank.append(j + 1)
-            if arr[i] == arrCopy[j]:
-                arrRank.append(j + 1)
-                seen.append(arr[i])
-
-    return arrRank
-
+    pass
 
 print(arrayRankTransform(arr))
