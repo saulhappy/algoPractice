54e97c33c184167b18c4af7ea4f03d32be1912c1
Saul Feliz <646634@gmail.com>
2020-11-17 10:49:12 -0600

    solved python/miscPracticeProblems/Algorithms/lrgstSubStr.py

--- python/miscPracticeProblems/Algorithms/lrgstSubStr.py
+++ python/miscPracticeProblems/Algorithms/lrgstSubStr.py
@@ -4,13 +4,20 @@ https://leetcode.com/problems/largest-substring-between-two-equal-characters/
 s = "cabbac"
 
 def maxLengthBetweenEqualCharacters(s):
-    lastSeen = {}
+    firstSeen = {}
     maxDist = -1
     
     for index, char in enumerate(s):
-        if char not in lastSeen:
-            lastSeen[char] = index
-    return lastSeen
+        if char not in firstSeen:
+            firstSeen[char] = index
+        else:
+            maxDist = max(index - firstSeen[char], maxDist)
+
+    if maxDist == -1:
+        return maxDist
+    else:
+        return maxDist - 1
+
 
 print(maxLengthBetweenEqualCharacters(s))
 
