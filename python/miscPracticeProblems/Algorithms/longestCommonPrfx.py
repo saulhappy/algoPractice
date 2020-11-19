56764f192084ba3a97739447777d55bbf371bad1
Saul Feliz <646634@gmail.com>
2020-11-19 13:18:13 -0600

    wrote out algorithm: 5 mins

--- python/miscPracticeProblems/Algorithms/longestCommonPrfx.py
+++ python/miscPracticeProblems/Algorithms/longestCommonPrfx.py
@@ -3,3 +3,18 @@ source: https://leetcode.com/problems/longest-common-prefix/
 
 """
 
+"""
+algorithm to try: take the first element of str, split.
+For each char is that first split element:
+If that char is equal to the char of the split element of next str, 
+add to result string. If not, continue. Return the string at the end of the iteration.
+
+"""
+
+strs = ["flower","flow","flight"]
+
+def longestCommonPrefix(strs):
+    pass
+
+print(longestCommonPrefix(str))
+
