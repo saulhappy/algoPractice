dea9ba7c63ff74a4569552dacb83377e880085c0
Saul Feliz <646634@gmail.com>
2020-11-20 18:35:50 -0600

    first pass at longestCommonPrfx.py

--- python/miscPracticeProblems/Algorithms/longestCommonPrfx.py
+++ python/miscPracticeProblems/Algorithms/longestCommonPrfx.py
@@ -1,9 +1,6 @@
 """
 source: https://leetcode.com/problems/longest-common-prefix/
 
-"""
-
-"""
 algorithm to try: take the first element of str, split.
 For each char is that first split element:
 If that char is equal to the char of the split element of next str, 
@@ -11,10 +8,23 @@ add to result string. If not, continue. Return the string at the end of the iter
 
 """
 
-strs = ["flower","flow","flight"]
+strs = ["saul","flower","flow","flight"]
+ 
 
 def longestCommonPrefix(strs):
-    pass
+    result = ""
 
-print(longestCommonPrefix(str))
+    for i in range(len(strs)):
+        currentWord = strs[i]
+        for j in range(len(currentWord)):
+            if currentWord[j] == strs[i + 1][j]:
+                result += currentWord[j]
+            else:
+                continue
+    return result
 
+
+
+
+
+print(longestCommonPrefix(strs))
