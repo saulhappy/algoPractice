cae6410ccfa04d516ad583e6411ca3231c2cd877
Saul Feliz <646634@gmail.com>
2020-11-22 16:02:24 -0600

    try at longestCommonPrfx.py

--- python/miscPracticeProblems/Algorithms/longestCommonPrfx.py
+++ python/miscPracticeProblems/Algorithms/longestCommonPrfx.py
@@ -4,12 +4,21 @@ source: https://leetcode.com/problems/longest-common-prefix/
 
 """
 
-strs = ["saul","flower","flow","flight"]
+strs = ["flower","flower","flower"]
  
 
 def longestCommonPrefix(strs):
-    result = ""
-    pass
+    lngstCommonPrefix = ""
 
+    if strs is None or len(strs) == 0:
+        return lngstCommonPrefix
+    
+    for i in range(len(strs[0])):
+        for j in range(1, len(strs)):
+            if i >= len(strs[i]) or strs[0][i] != strs[j][i]:
+                return lngstCommonPrefix
+        lngstCommonPrefix += strs[0][i]
+    
+    return lngstCommonPrefix
 
 print(longestCommonPrefix(strs))
