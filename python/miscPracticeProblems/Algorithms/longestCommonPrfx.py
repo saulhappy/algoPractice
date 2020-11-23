f510651a4a900c26cc69b774c029588808a7485e
Saul Feliz <646634@gmail.com>
2020-11-23 06:20:47 -0600

    solved python/miscPracticeProblems/Algorithms/longestCommonPrfx.py

--- python/miscPracticeProblems/Algorithms/longestCommonPrfx.py
+++ python/miscPracticeProblems/Algorithms/longestCommonPrfx.py
@@ -4,21 +4,21 @@ source: https://leetcode.com/problems/longest-common-prefix/
 
 """
 
-strs = ["flower","flower","flower"]
+strs = ["flower","flight","flower", "saul"]
  
 
 def longestCommonPrefix(strs):
-    lngstCommonPrefix = ""
-
     if strs is None or len(strs) == 0:
-        return lngstCommonPrefix
-    
-    for i in range(len(strs[0])):
-        for j in range(1, len(strs)):
-            if i >= len(strs[i]) or strs[0][i] != strs[j][i]:
-                return lngstCommonPrefix
-        lngstCommonPrefix += strs[0][i]
-    
+        return ""
+        
+    lngstCommonPrefix = strs[0]
+
+    for i in range(1, len(strs)):
+        while lngstCommonPrefix != strs[i][0 : len(lngstCommonPrefix)]:
+            lngstCommonPrefix = lngstCommonPrefix[0:len(lngstCommonPrefix) - 1]
+            if len(lngstCommonPrefix) == 0:
+                return ""
+
     return lngstCommonPrefix
 
 print(longestCommonPrefix(strs))
