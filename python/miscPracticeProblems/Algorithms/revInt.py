ccb33a75c63e6db19d435b63f3432ad8f14d7639
Saul Feliz <646634@gmail.com>
2020-11-25 06:00:46 -0600

    solved revInt.py

--- python/miscPracticeProblems/Algorithms/revInt.py
+++ python/miscPracticeProblems/Algorithms/revInt.py
@@ -3,10 +3,29 @@ source: https://leetcode.com/problems/reverse-integer/
 
 """
 
-integer = -123
+"""
+first algorithm to try:
+turn int to string, iterate, for each char starting from the back, append to new string, then convert to int.
+edge cases: zero, postive, negative
 
-def reverseInt(integer):
-    pass
+"""
+
+x = 1534236469
+
+def reverseInt(x):
+    result = 0
+
+    if x == 0:
+        return result
+    if x > 0:
+        result = int(str(x)[::-1])
+    if x < 0:
+        result = int(("-" + str(x * -1)[::-1]))
+
+    if result <= -2**31 or result >= 2**31:
+        return 0
+    else:
+        return result
 
 
-print(reverseInt(integer))
+print(reverseInt(x))
--- python/scratch.py
+++ python/scratch.py
@@ -1,17 +1,1 @@
-admins = [
-    ("""Scott White""", "scott.white@huvrdata.com"),
-    ("""Jeff Sheffield""", "jeff.sheffield@huvrdata.com"),
-    ("""Nick Fang""", "nick.fang@huvrdata.com"),
-    ("""Julia Guimiot""", "julia.guimiot@huvrdata.com"),
-    ("""Sean Hong""", "sean.hong@huvrdata.com"),
-    ("""Saul Feliz""", "saul.feliz@huvrdata.com"),
-    ("""Steve Hatch""", "steve.hatch@huvrdata.com"),
-    ("""William Wigfield""", "william.wigfield@huvrdata.com"),
-]
-
-# admin_emails = []
-# for admin in admins:
-#     admin_emails.append(admin[1])
-
-admin_emails = [admin[1] for admin in admins]
-print(admin_emails)
+print(2**31)
