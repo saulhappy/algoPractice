cbdfd87ba3ff0cec34263d884e60739fed9f7086
Saul Feliz <646634@gmail.com>
2020-11-27 12:49:27 -0600

    solved palindromeNum.py

--- python/miscPracticeProblems/Algorithms/palindromeNum.py
+++ python/miscPracticeProblems/Algorithms/palindromeNum.py
@@ -4,6 +4,9 @@ source: https://leetcode.com/problems/palindrome-number/
 x = -121
 
 def isPalindrome(x):
-    pass
+    if x < 0:
+        return False
+    else:
+        return x == int(str(x)[::-1])
 
 print(isPalindrome(x))
