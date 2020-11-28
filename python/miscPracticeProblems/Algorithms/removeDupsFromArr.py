a8bcdda830e3dcd40d0a1d2fdf6652cf894f2005
Saul Feliz <646634@gmail.com>
2020-11-28 08:34:10 -0600

    added removeDupsFromArr.py with algo

---
+++ python/miscPracticeProblems/Algorithms/removeDupsFromArr.py
@@ -0,0 +1,15 @@
+"""
+source: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+"""
+nums = [0,0,1,1,1,2,2,3,3,4]
+# output: 5
+
+""" 
+algorithm to try: create a new set from input, and return the length of that
+
+"""
+def removeDuplicates(nums):
+    pass
+
+print(removeDuplicates(nums))
