c5b1c275f4909f840bb5eca81ab70f2f1c33e1e7
Saul Feliz <646634@gmail.com>
2020-12-02 07:56:32 -0600

    second pass at removeDupsFromArr.py

--- python/miscPracticeProblems/Algorithms/removeDupsFromArr.py
+++ python/miscPracticeProblems/Algorithms/removeDupsFromArr.py
@@ -5,21 +5,18 @@ source: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 nums = [0,0,1,1,1,2,2,3,3,4]
 # output: 5
 
-""" 
-algorithm to try: create a new set from input, and return the length of that
-
-"""
 def removeDuplicates(nums):
-    if len(nums) == 0:
-        return 0
-    j = 1
-    result = 1
-
-    for i in range(len(nums)):
-        if  nums[i] == nums[j]:
-            continue
+    i = 0
+    while True:
+        try:
+            j = nums[i + 1]
+        except IndexError:
+            break
+        if nums[i] == nums[j]:
+            del nums[i]
         else:
-            result += 1
-    return result
+            i += 1
+    return len(nums)
+
 
 print(removeDuplicates(nums))
