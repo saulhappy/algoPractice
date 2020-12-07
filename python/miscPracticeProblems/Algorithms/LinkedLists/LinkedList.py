a998a18c2119218603cd26acb4bd2d6ee55c9c94
Saul Feliz <646634@gmail.com>
2020-12-07 09:54:15 -0600

    started linked lists w/python

---
+++ python/miscPracticeProblems/Algorithms/LinkedLists/LinkedList.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+class node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = node()
+
+
+
