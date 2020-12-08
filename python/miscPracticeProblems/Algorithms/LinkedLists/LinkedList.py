882e0993a409b5f3cf20152090d349119cb6d19d
Saul Feliz <646634@gmail.com>
2020-12-08 08:38:15 -0600

    added append, length functions to linked list implementation

--- python/miscPracticeProblems/Algorithms/LinkedLists/LinkedList.py
+++ python/miscPracticeProblems/Algorithms/LinkedLists/LinkedList.py
@@ -8,5 +8,32 @@ class linked_list:
     def __init__(self):
         self.head = node()
 
+    def append(self, data):
+        new_node = node(data)
+        current_node = self.head
+
+        while current_node.next != None:
+            current_node = current_node.next
+        current_node.next = new_node
+    
+    def length(self):
+        current_node = self.head
+        node_count = 0
+
+        while current_node.next != None:
+            node_count += 1
+            current_node = current_node.next
+        return node_count
 
 
+
+a = node("a")
+b = node("b")
+c = node("c")
+
+l = linked_list()
+l.append(a)
+l.append(b)
+l.append(c)
+
+print(l.length())
