54a54b009e5df2d30c7bc64603499393682d4957
Saul Feliz <646634@gmail.com>
2020-12-11 05:35:51 -0600

    first pass at python/miscPracticeProblems/Algorithms/Misc/strStr.py

--- python/miscPracticeProblems/Algorithms/Misc/strStr.py
+++ python/miscPracticeProblems/Algorithms/Misc/strStr.py
@@ -1,11 +1,28 @@
 """ source: https://leetcode.com/problems/implement-strstr/ """
 
-haystack = "hello"
-needle = "ll"
+haystack = ""
+needle = ""
 
 
 def strStr(haystack, needle):
-    pass
+    if needle not in haystack:
+        return -1
+
+    if len(needle) == 0:
+        return 0
+
+    for i in range(len(haystack)):
+        index = 0
+        if needle[0] == haystack[i]:
+            index = i
+            for j in range(1, len(needle)):
+                if needle[j] != haystack[i + 1]:
+                    return -1
+        else:
+            continue
+        return index
+
+    return "it's in there"
 
 
 print(strStr(haystack, needle))
