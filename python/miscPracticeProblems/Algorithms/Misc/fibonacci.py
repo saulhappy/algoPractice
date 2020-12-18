1388ca0670ef63f6d11c09488a65bd54e0500656
Saul Feliz <646634@gmail.com>
2020-12-18 10:30:26 -0600

    started fib normal recursion + calculations

--- python/miscPracticeProblems/Algorithms/Misc/fibonacci.py
+++ python/miscPracticeProblems/Algorithms/Misc/fibonacci.py
@@ -1,10 +1,10 @@
 """ return nth number of fibonnaci sequence """
+import time
+fib_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887,9227465, 14930352, 24157817, 39088169]
 
-fib_sequnce = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811]
-
+n = 38
 
 # normal recursive:
-
 def fibonnaci_rec(n):
     if n <= 0:
         return None
@@ -13,5 +13,19 @@ def fibonnaci_rec(n):
     else:
         return fibonnaci_rec(n - 1) + fibonnaci_rec(n - 2)
 
-print(fib_sequnce[28])
-print(f"fib of 28 is {fibonnaci_rec(28)}")
+
+
+
+# outputs
+
+start = time.time()
+print(f"the {n}th number of the fibonnaci sequence is {fib_sequence[n]}")
+end = time.time()
+print(f"look up of the {n}th number took {end - start} seconds")
+
+start = time.time()
+fibonnaci_rec(n)
+end = time.time()
+
+print(f"calculating the {n}th number with normal recursion took {end - start} seconds")
+
