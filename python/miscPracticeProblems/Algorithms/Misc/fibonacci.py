e4bf98a217c2e32daf7d96a6529a9942b79d05cb
Saul Feliz <646634@gmail.com>
2020-12-20 11:39:39 -0600

    attempt at fibonacci memoized in py

--- python/miscPracticeProblems/Algorithms/Misc/fibonacci.py
+++ python/miscPracticeProblems/Algorithms/Misc/fibonacci.py
@@ -2,10 +2,11 @@
 import time
 fib_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887,9227465, 14930352, 24157817, 39088169]
 
-n = 38
+n = 7
 
 # normal recursive:
 def fibonnaci_rec(n):
+
     if n <= 0:
         return None
     elif n == 1 or n == 2:
@@ -13,8 +14,23 @@ def fibonnaci_rec(n):
     else:
         return fibonnaci_rec(n - 1) + fibonnaci_rec(n - 2)
 
+# normal recursive + memo:
+def fibonnaci_rec_memo(n):
+    memo = {
+        0 : 0,
+        1 : 1, 
+        2 : 1
+    }
 
-
+    if n <= 0:
+        return None
+    elif n == 1 or n == 2:
+        return 1
+    elif n in memo.keys():
+        return memo[n]
+    else:
+        memo[n] = fibonnaci_rec_memo(n - 1) + fibonnaci_rec_memo(n - 2)
+        return memo[n]
 
 # outputs
 
@@ -29,3 +45,8 @@ end = time.time()
 
 print(f"calculating the {n}th number with normal recursion took {end - start} seconds")
 
+start = time.time()
+fibonnaci_rec_memo(n)
+end = time.time()
+
+print(f"calculating the {n}th number with normal recursion wtih memo took {end - start} seconds")
--- python/scratch.py
+++ python/scratch.py
@@ -1,5 +1,32 @@
-arr = [1, 2, 3, 4]
+# my_dict = {
+#     1: 'a',
+#     2: 'b',
+#     3: 'c'
+# }
 
-# mid = arr[len(arr)/2]
+# my_dict[4] = 'd'
 
-print((type(len(arr)/2)))
+# print(my_dict)
+
+# normal recursive + memo:
+
+n = 38
+def fibonnaci_rec_memo(n):
+    memo = {
+        0 : 0,
+        1 : 1, 
+        2 : 1
+    }
+
+    if n <= 0:
+        return None
+    elif n == 1 or n == 2:
+        return 1
+    elif n in memo.keys():
+        return memo[n]
+    else:
+        memo[n] = fibonnaci_rec_memo(n - 1) + fibonnaci_rec_memo(n - 2)
+        return memo[n]
+
+
+print(fibonnaci_rec_memo(n))
