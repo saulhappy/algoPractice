5ddf0176c686a7db403fa1ab71e70cba546c3278
Saul Feliz <646634@gmail.com>
2020-12-23 07:22:45 -0600

    added grid traveler

---
+++ python/miscPracticeProblems/Algorithms/Misc/grid_traveler.py
@@ -0,0 +1,13 @@
+""" looks same as leetcode unique paths 
+source: https://leetcode.com/problems/unique-paths/ 
+given a c x r grid, where you are located in the top left corner, and can only move left and right,
+how many paths can  you take to reach the bottom right corner?
+"""
+
+c = 2
+r = 3
+def grid_traveler(c, r):
+    pass
+
+print(grid_traveler(c, r))
+
