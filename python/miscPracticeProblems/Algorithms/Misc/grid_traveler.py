8eca210ece8d0bf5b5bd7b3ba2e7e72e711d8bed
Saul Feliz <646634@gmail.com>
2020-12-27 14:59:34 -0600

    first pass at grid traveler

--- python/miscPracticeProblems/Algorithms/Misc/grid_traveler.py
+++ python/miscPracticeProblems/Algorithms/Misc/grid_traveler.py
@@ -4,10 +4,25 @@ given a c x r grid, where you are located in the top left corner, and can only m
 how many paths can  you take to reach the bottom right corner?
 """
 
-c = 2
-r = 3
-def grid_traveler(c, r):
-    pass
+c = 15
+r = 15
+
+"""
+Going down reduces playable areas by a row, going right by a column.
+At 1x1, we do nothing: start is same as end, or 1
+At any 0 coordinate, there's nothing to do: it's not a grid, or 0 
+
+"""
+
+def grid_traveler(r, c):
+    if c == 1 and r == 1:
+        return 1
+    if c == 0 or r == 0:
+        return 0
+    else:
+        return grid_traveler(r - 1, c) + grid_traveler(r, c -1)
+    
+    
 
 print(grid_traveler(c, r))
 
