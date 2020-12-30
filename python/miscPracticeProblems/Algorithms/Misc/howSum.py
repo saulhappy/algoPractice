a960952078144540a55d0dc95fa47796d0b82ce9
Saul Feliz <646634@gmail.com>
2020-12-30 14:53:49 -0600

    solved howSum.py with brute force

--- python/miscPracticeProblems/Algorithms/Misc/howSum.py
+++ python/miscPracticeProblems/Algorithms/Misc/howSum.py
@@ -11,3 +11,24 @@ howSum(0, [1, 2, 3]) => []
 """
 
 
+
+def howSum(targetSum, numbers):
+    if targetSum == 0: return []
+    if targetSum < 0: return None
+
+    for num in numbers:
+        complement = targetSum - num
+        complement_result = howSum(complement, numbers)
+        if complement_result != None:
+            return complement_result + [num]
+    return None
+
+
+
+print(howSum(7, [2, 3])) # => [3, 2, 2]
+print(howSum(7, [5, 3, 4, 7])) # => [4, 3]
+print(howSum(7, [2, 4])) # => null
+print(howSum(8, [2, 3, 5])) # => [2, 2, 2, 2]
+# print(howSum(300, [7, 14])) # => null
+
+
