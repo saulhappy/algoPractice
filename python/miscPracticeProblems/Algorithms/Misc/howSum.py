c4de6e44a177ce1f85773cb7b94b2596c73218d4
Saul Feliz <646634@gmail.com>
2020-12-30 14:08:59 -0600

    added howSum.py

---
+++ python/miscPracticeProblems/Algorithms/Misc/howSum.py
@@ -0,0 +1,13 @@
+"""
+Write a function howSum that takes in a targetSum and an array of numbers as arguments. 
+The function should return an array containing any combination of elements that add up to exactly the targetSum. 
+If there is no combination that adds up to the targetSum, return null. 
+If there are multiple combinations possible, you may return any single one. 
+
+e.g. howSum(7, [5, 3, 4, 7]) => [3, 4] or [7]
+howSum(8, [2, 3, 5]) => [2, 2, 2, 2] or [3, 5]
+howSum(7, [2, 4]) => null
+howSum(0, [1, 2, 3]) => []
+"""
+
+
