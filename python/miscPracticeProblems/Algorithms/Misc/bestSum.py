b3effb4874fc70efa6b1ea2b533cec4e72474798
Saul Feliz <646634@gmail.com>
2020-12-30 15:37:15 -0600

    added bestSum.py

---
+++ python/miscPracticeProblems/Algorithms/Misc/bestSum.py
@@ -0,0 +1,18 @@
+"""
+write a function bestSum(targetSum, numbers) that takes in a targetsum and array of numbers as arguments.
+The function should return an array containing the SHORTEST combination of numbers that add up to exactly the targetSum. 
+If there is a time for the shortest combination, you may return any one of the shortest. 
+
+bestSum(7, [5, 3, 4, 7]) => [7]
+bestSum(8, [2, 3, 5]) => [3, 5]
+
+"""
+
+def bestSum(targetSum, numbers):
+    pass
+
+
+print(bestSum(7, [5, 3, 4, 7])) # => [7]
+print(bestSum(8, [2, 3, 5])) # => [3, 5]
+
+
