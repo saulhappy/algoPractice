55ff2d181c16484936afa54e397a3b48718f1209
Saul Feliz <646634@gmail.com>
2021-01-02 06:05:14 -0600

    solved bestSum memoized

--- python/miscPracticeProblems/Algorithms/Misc/bestSum.py
+++ python/miscPracticeProblems/Algorithms/Misc/bestSum.py
@@ -25,12 +25,38 @@ def bestSum(targetSum, numbers):
             shortest_complements = combination
     return shortest_complements
 
+
+def bestSumMemo(targetSum, numbers, memo = {}):
+    if targetSum in memo:
+        return memo[targetSum] 
+    if targetSum == 0: return []
+    if targetSum < 0: return None
+
+    shortest_complements = None
+    combination = None
+
+    for num in numbers:
+        complement = targetSum - num
+        complements = bestSumMemo(complement, numbers, memo)
+
+        if complements != None:
+             combination = complements + [num]
+        if shortest_complements == None or len(combination) < len(shortest_complements):
+            shortest_complements = combination
+
+    memo[targetSum] = shortest_complements
+    return shortest_complements
+
 """
 target sum: m
 n : len(numbers)
 
 Brute force:
-time: O(n^m*m)
+time: O(n^m * m)
+space: O(m^2)
+
+Memoized:
+time: O(m^2 * n )
 space: O(m^2)
 
 """
@@ -39,5 +65,7 @@ space: O(m^2)
 # print(bestSum(8, [2, 3, 5])) # => [3, 5]
 # print(bestSum(8, [1, 4, 5])) # => [4, 4]
 # print(bestSum(100, [1, 2, 5, 25])) # => [25, 25, 25, 25]
+
+print(bestSumMemo(100, [1, 2, 5, 25])) # => [25, 25, 25, 25]
 
 
