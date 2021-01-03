cd9e96c997cd68663e071578ff65a69f71d1d5b1
Saul Feliz <646634@gmail.com>
2021-01-03 10:26:31 -0600

    added findCommon.py problem

---
+++ python/miscPracticeProblems/Algorithms/Misc/findCommon.py
@@ -0,0 +1,13 @@
+"""
+find the common elements in a sorted array with distinct elements
+"""
+
+arr1 = [1, 5, 15, 20, 30, 37, 42]
+arr2 = [2, 3, 4, 5, 25, 37, 40]
+
+
+def findCommon(arr1, arr2):
+    pass
+
+print(findCommon(arr1, arr2))
+
