4ee66a01dfe1959d3cd4be5f0b31a7478a3ab998
Saul Feliz <646634@gmail.com>
2021-01-09 17:14:23 -0600

    first_unique.py setup

---
+++ JavaScript/scratch.js
@@ -0,0 +1,9 @@
+const roleOptions = [
+    { label: 'Subcontractor', value: 'SUBCONTRACTOR'},
+    { label: 'Asset Owner', value: 'OWNER'},
+    { label: 'Service Provider', value: 'SERVICE'},
+  ]
+
+  roleOptions.map(value =>{
+      console.log(value.label)
+  })
---
+++ python/miscPracticeProblems/Algorithms/Misc/first_unique.py
@@ -0,0 +1,10 @@
+# Given a string, find the first non-repeating character in it and return its index. 
+# If it doesn't exist, return -1. # Note: all the input strings are already lowercase.
+
+string = "this is a string"
+
+def first_unique(string): 
+    pass
+
+print(first_unique(string))
+
