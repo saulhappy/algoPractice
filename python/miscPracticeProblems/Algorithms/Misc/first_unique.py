d0146b413e0119bb92523165a75da7126130a6cd
Saul Feliz <646634@gmail.com>
2021-01-10 21:18:39 -0600

    added algo for first_unique.py

--- python/miscPracticeProblems/Algorithms/Misc/first_unique.py
+++ python/miscPracticeProblems/Algorithms/Misc/first_unique.py
@@ -1,7 +1,19 @@
 # Given a string, find the first non-repeating character in it and return its index. 
 # If it doesn't exist, return -1. # Note: all the input strings are already lowercase.
 
-string = "this is a string"
+
+"""
+Algorithm to try:
+if len of string == 0, return -1
+create array of special characters. Create hash. Iterate.
+If current char is in special character array, continue. 
+Add char with char as key, 1 or += 1 as value. 
+End of iteration
+Iterate string again with enumerate. 
+If char in hash with value of 1, return index. 
+"""
+
+string = "hi! i am a string!"
 
 def first_unique(string): 
     pass
--- python/scratch.py
+++ python/scratch.py
@@ -1,1 +1,7 @@
-print(ord("9"))
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 10]
+
+for num in arr:
+    if num % 2 == 0: 
+        continue
+    print(num)
+
