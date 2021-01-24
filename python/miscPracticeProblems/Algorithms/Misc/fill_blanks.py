648d2eabace72ac959416bdbbeac4f60849bfe8b
Saul Feliz <646634@gmail.com>
2021-01-24 08:33:04 -0600

    added python/miscPracticeProblems/Algorithms/Misc/fill_blanks.py

---
+++ python/miscPracticeProblems/Algorithms/Misc/fill_blanks.py
@@ -0,0 +1,10 @@
+# Given an array containing None values fill in the None values with most recent 
+# non None value in the array 
+
+array1 = [1,None,2,3,None,None,5,None] # => [1, 1, 2, 3, 3, 3, 5, 5]
+
+
+def fill_blanks(arr):
+    pass
+
+print(fill_blanks(array1))
