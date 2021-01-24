a402e168aa232648f8229b46dd5d7f8e5a1e0f29
Saul Feliz <646634@gmail.com>
2021-01-23 19:24:08 -0600

    solved python/miscPracticeProblems/Algorithms/Misc/move_zeros.py

--- python/miscPracticeProblems/Algorithms/Misc/move_zeros.py
+++ python/miscPracticeProblems/Algorithms/Misc/move_zeros.py
@@ -4,3 +4,21 @@
 array1 = [0,1,0,3,12] # => [1, 3, 12, 0, 0]
 array2 = [1,7,0,0,8,0,10,12,0,4] # => [1, 7, 8, 10, 12, 4, 0, 0, 0, 0]
 
+"""
+algorithm to try:
+for each element of arr,
+  if element == 0, pop it, then append to a result arr
+at the end , return result arr
+"""
+
+def move_zeros(arr):
+
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            arr.pop(i)
+            arr.append(0)
+    return arr
+
+
+
+print(move_zeros(array2))
