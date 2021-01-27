7646cab65147425ff3b0593725302ddc91676537
Saul Feliz <646634@gmail.com>
2021-01-27 09:30:09 -0600

    added match_mismatch problem

---
+++ python/miscPracticeProblems/Algorithms/Misc/match_mismatch.py
@@ -0,0 +1,16 @@
+#Given two sentences, return an array that has the words that appear in one sentence and not
+#the other and an array with the words in common. 
+
+sentence1 = 'We are really pleased to meet you in our city'
+sentence2 = 'The city was hit by a really heavy storm'
+
+# Output:
+# (['The','We','a','are','by','heavy','hit','in','meet','our',
+#     'pleased','storm','to','was','you'],
+#  ['city', 'really'])
+
+def match_mismatch(string1, string2):
+    pass
+
+print(match_mismatch(sentence1, sentence2))
+
