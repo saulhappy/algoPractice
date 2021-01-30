ca18b69db6321c6fd63edc556e05d1fad6a0ebca
Saul Feliz <646634@gmail.com>
2021-01-30 15:22:11 -0600

    added prime_nums.py problem"

---
+++ python/miscPracticeProblems/Algorithms/Misc/prime_nums.py
@@ -0,0 +1,14 @@
+# Given k numbers which are less than n, return the set of prime number among them
+# Note: The task is to write a program to print all Prime numbers in an Interval.
+# Definition: A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
+
+
+n = 35 # => [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+def prime_nums(n):
+    pass
+
+
+
+
+
