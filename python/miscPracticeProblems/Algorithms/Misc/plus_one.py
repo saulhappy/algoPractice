5f753b8343296f9a71a3b551e70b565a3a43caa4
Saul Feliz <646634@gmail.com>
2021-02-04 05:41:01 -0600

    second pass at plus_one.py

--- python/miscPracticeProblems/Algorithms/Misc/plus_one.py
+++ python/miscPracticeProblems/Algorithms/Misc/plus_one.py
@@ -24,11 +24,24 @@ Output: [1]
 Algorithm 1: 
 create empty string, and join digits into the string. Convert to integer and add 1. 
 Iterate the string, converting each char back to num, and append to results array. 
+
+Works for most uses cases, but not edge case: [0,0] (doesn't seem like a real num?)
+Output is [1], expected is [0, 1]. [0,0,0] => [0, 0, 1]; [0, 0, 9] => [0, 1, 0]
 """
 
-digits = [0]
+"""
+Algorithm 2:
+create copy of digits
+Pop the last element of copy. 
+If it's 9, pop last element again, add 1, and append back to copy. 
+Else, add one, append back to copy
+return copy.
 
-def plusOne1(digits):
+"""
+
+digits = [1, 9]
+
+def plus_One_1(digits):
     digits_string = ""
     result = []
     
@@ -42,6 +55,28 @@ def plusOne1(digits):
     
     return result
 
+def plus_One_2(digits):
+    result = digits[:]
 
-print(plusOne1(digits))
+    last_el = result.pop()
+
+    if last_el == 9:
+        if len(result) == 0:
+            return [1, 0]
+        else:
+            second_last = result.pop()
+            last_two = str(second_last) + str(last_el)
+            last_two = str(int(last_two) + 1)
+            result.append(int(last_two[0]))
+            result.append(int(last_two[1]))
+            return result
+    else:
+        last_el += 1
+        result.append(last_el)
+        return result
+
+
+
+
+print(plus_One_2(digits))
 
