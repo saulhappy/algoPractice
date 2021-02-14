9399884d2043a85f5c8653928449697e0d8861c5
Saul Feliz <646634@gmail.com>
2021-02-13 19:56:14 -0600

    update on stock_merchant.py

--- python/miscPracticeProblems/Algorithms/Misc/sock_merchant.py
+++ python/miscPracticeProblems/Algorithms/Misc/sock_merchant.py
@@ -2,19 +2,23 @@ arr=[1,2,1,2,1,3,2]
 n = 7
 
 def sock_merchant(arr, n):
-    pairs = 0
-    socks = {}
+    # pairs = 0
+    # socks = {}
 
-    for sock in arr:
-        if sock in socks:
-            socks[sock] += 1
-        else:
-            socks[sock] = 1
+    # for sock in arr:
+    #     if sock in socks:
+    #         socks[sock] += 1
+    #     else:
+    #         socks[sock] = 1
 
-    for value in socks.values():
-        pairs += value // 2
+    # for value in socks.values():
+    #     pairs += value // 2
     
-    return pairs
+    # return pairs
+
+# MORE PYTHONIC USING COLLECTIONS
+
+    from collections import Counter
+    return sum(sock // 2 for sock in Counter(arr).values())
 
 print(sock_merchant(arr, n))
-
