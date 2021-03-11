00e2cdf3edb5d17620a7379bfd400da7de4649ae
Saul Feliz <646634@gmail.com>
2021-03-11 15:34:54 -0600

    starting out bst

---
+++ python/miscPracticeProblems/Graphs/bst.py
@@ -0,0 +1,13 @@
+# custom implementation of binary search tree
+
+class Node():
+    def __init__(self, data = None):
+        self.data = data
+
+class BST():
+    def __init__(self):
+        self.root = Node()
+        self.count = 1
+
+
+
