87d6f10595bd32bc53748f2cefc988d224887b0a
Saul Feliz <646634@gmail.com>
2021-03-14 10:16:36 -0500

    added private insert method to bst.py

--- python/miscPracticeProblems/Graphs/bst.py
+++ python/miscPracticeProblems/Graphs/bst.py
@@ -1,8 +1,8 @@
 # custom implementation of binary search tree
 
 class Node():
-    def __init__(self, data = None):
-        self.data = data
+    def __init__(self, value = None):
+        self.value = value
         self.right = None
         self.left = None
 
@@ -12,9 +12,30 @@ class BST():
         self.count = 0
 
 
-    def insert(self, data):
+    # if there is no root, create it, increment the count. else, call private insert with the value. 
+    # private methods should not be called outside of the class
+    def insert(self, value):
         if self.root == None:
-            self.root = Node(data)
+            self.root = Node(value)
             self.count += 1
+        else:
+            self._insert(value, self.root)
+    
+    # recursive private insert method:
+    def _insert(self, value, current_node):
+        if value < current_node.value:
+            if current_node.left == None:
+                current_node.left = Node(value)
+            else: 
+                self._insert(value, current_node.left)
+        elif value > current_node.value:
+            if current_node.right == None:
+                current_node.right = Node(value)
+            else:
+                self._insert(value, current_node.right)
+        else:
+            print("This value is already in the tree")
+
+
     
 
