980545aa153c8c5880ca6b486f6ac18d48ed8b5d
Saul Feliz <646634@gmail.com>
2021-04-02 16:28:31 -0500

    added Algorithms/Misc/contains_duplicate.py

---
+++ python/miscPracticeProblems/Algorithms/Misc/contains_duplicate.py
@@ -0,0 +1,22 @@
+""" 
+source: https://leetcode.com/problems/contains-duplicate/ 
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+
+"""
+nums = [1,2,3,1]
+
+def contains_duplicate(nums):
+    pass
+
+
+print(contains_duplicate(nums))
