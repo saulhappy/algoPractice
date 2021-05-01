3d0f723e7e22ba528876ab04a9ae4002a3b89c37
Saul Feliz <646634@gmail.com>
2021-05-01 05:14:05 -0500

    two_D01.py problem set up

---
+++ python/miscPracticeProblems/Algorithms/Misc/two_D01.py
@@ -0,0 +1,14 @@
+"""
+source: https://www.youtube.com/watch?v=UbBCg0-xjkU
+Write a program that reads a 5x5 array of integers and then prints the row sum and column sum. 
+
+arr = [
+    [8,3,9,0,10],
+    [3,5,17,1,1],
+    [2,8,6,23,1],
+    [15,7,3,2,9],
+    [6,14,2,6,0]
+]
+=> row total: 30, 27, 40, 36, 28
+=> column total: 34, 37, 37, 32, 21
+"""
