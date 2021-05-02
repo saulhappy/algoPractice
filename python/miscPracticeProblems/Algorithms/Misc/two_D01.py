ec5fa3bad6902a0c4200a220e3fb984dbf8fd130
Saul Feliz <646634@gmail.com>
2021-05-02 12:31:51 -0500

    started on two_D01.py

--- python/miscPracticeProblems/Algorithms/Misc/two_D01.py
+++ python/miscPracticeProblems/Algorithms/Misc/two_D01.py
@@ -12,3 +12,27 @@ arr = [
 => row total: 30, 27, 40, 36, 28
 => column total: 34, 37, 37, 32, 21
 """
+
+arr = [
+    [8,3,9,0,10],
+    [3,5,17,1,1],
+    [2,8,6,23,1],
+    [15,7,3,2,9],
+    [6,14,2,6,0]
+]
+
+def sum_row_col(arr):
+    row_total = []
+    col_total = []
+    for row in arr:
+        row_total.append(sum(row))
+    for i in range(len(arr)):
+        curr_col =[arr[i][i]]
+        for j in range(i + 1, 5):
+            curr_col.append(arr[j][i])
+
+    
+    return f"row total: {row_total} \ncolumn total: {col_total}"
+            
+
+print(sum_row_col(arr))
