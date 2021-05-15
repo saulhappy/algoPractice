620964018f5e2f9b4d46db8eace884dd55fa6900
Saul Feliz <646634@gmail.com>
2021-05-15 13:39:27 -0500

    added sort_nums.py

---
+++ python/miscPracticeProblems/Algorithms/Misc/sort_colors.py
@@ -0,0 +1,22 @@
+"""
+source: https://leetcode.com/problems/sort-colors/
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+
+"""
+# Test cases:
+nums1 = [2,0,2,1,1,0] # ==> [0,0,1,1,2,2]
+nums2 = [2,0,1] # ==> [0,1,2]
+nums3 = [0] # ==> [0]
+nums4 =  [1] # ==> [1]
+
+
+def sort_colors(nums):
+    pass
+
+print(sort_colors(nums1))
