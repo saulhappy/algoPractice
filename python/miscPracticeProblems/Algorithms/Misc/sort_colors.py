83455990acb68f3e5a7494e1b653b63305499892
Saul Feliz <646634@gmail.com>
2021-05-16 16:28:48 -0500

    dutch national flag solution for sort_colors.py

--- python/miscPracticeProblems/Algorithms/Misc/sort_colors.py
+++ python/miscPracticeProblems/Algorithms/Misc/sort_colors.py
@@ -16,7 +16,26 @@ nums3 = [0] # ==> [0]
 nums4 =  [1] # ==> [1]
 
 
-def sort_colors(nums):
-    pass
+"""
+The algorithm is the dutch national flag algorithm: https://en.wikipedia.org/wiki/Dutch_national_flag_problem
+"""
 
-print(sort_colors(nums1))
+def sort_colors(nums):
+    low = 0
+    mid = 0
+    high = len(nums) - 1
+
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+    return nums
+        
+
+print(sort_colors(nums4))
