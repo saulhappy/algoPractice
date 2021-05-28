73fd34ecccb1421a2c26f3f76143f69a303d5623
Saul Feliz <646634@gmail.com>
2021-05-27 20:31:21 -0500

    setup set_matrix_zero.py

---
+++ python/miscPracticeProblems/Algorithms/Misc/set_matrix_zero.py
@@ -0,0 +1,16 @@
+"""
+source: https://leetcode.com/problems/set-matrix-zeroes/
+
+Given an m x n matrix. If an element is 0, set its entire row and column to 0. Do it in-place.
+
+"""
+
+# test cases:
+matrix1 = [[1,1,1],[1,0,1],[1,1,1]] # => [[1,0,1],[0,0,0],[1,0,1]]
+matrix2 = [[0,1,2,0],[3,4,5,2],[1,3,1,5]] # => [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+def setZeros(matrix):
+    pass
+
+
+print(setZeros(matrix1))
