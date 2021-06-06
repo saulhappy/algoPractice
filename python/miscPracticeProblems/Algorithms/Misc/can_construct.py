7ee224ef2d03ac5e4b9862e09ee6b28fb7855c59
Saul Feliz <646634@gmail.com>
2021-06-06 16:12:42 -0500

    can_construct.py problem setup

---
+++ python/miscPracticeProblems/Algorithms/Misc/can_construct.py
@@ -0,0 +1,15 @@
+"""
+Source: https://www.youtube.com/watch?v=oBt53YbR9Kk
+Write a function can_construct(target, wordBank) that accepts a target string and an array of strings. 
+The function should return a boolean indicating whether or not the 
+'target'can be constructed by concatenating elements of the 'wordbank' array. 
+You may reuse elemnts of 'wordbank' as many times as needed. 
+
+"""
+
+def can_construct(target, word_bank):
+    pass
+
+
+
+
