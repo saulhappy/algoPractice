45bb41a5d0425bb00aacf7862422cd4c19969a7b
Saul Feliz <646634@gmail.com>
2021-06-07 09:48:54 -0500

    added test cases can_construct.py

--- python/miscPracticeProblems/Algorithms/Misc/can_construct.py
+++ python/miscPracticeProblems/Algorithms/Misc/can_construct.py
@@ -7,6 +7,20 @@ You may reuse elemnts of 'wordbank' as many times as needed.
 
 """
 
+# test cases
+# target0 = 'abcdef'
+# wordbank0 = ['ab', 'abc', 'cd', 'def', 'abcd']
+# result0 => true
+
+# target1 = 'skateboard'
+# wordbank1 = ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']
+# result1 => false
+
+# target2 = ''
+# wordbank2 = ['cat', 'dog', 'mouse']
+# result2 => true
+
+
 def can_construct(target, word_bank):
     pass
 
