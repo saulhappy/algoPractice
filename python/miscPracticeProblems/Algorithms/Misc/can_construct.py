f5a0935b3b6cf1ecc32e7270a3d0dac22b88c3a3
Saul Feliz <646634@gmail.com>
2021-06-08 10:18:06 -0500

    brute force solution to can_construct.py

--- python/miscPracticeProblems/Algorithms/Misc/can_construct.py
+++ python/miscPracticeProblems/Algorithms/Misc/can_construct.py
@@ -8,22 +8,43 @@ You may reuse elemnts of 'wordbank' as many times as needed.
 """
 
 # test cases
-# target0 = 'abcdef'
-# wordbank0 = ['ab', 'abc', 'cd', 'def', 'abcd']
+
+target0 = 'abcdef'
+wordbank0 = ['ab', 'abc', 'cd', 'def', 'abcd']
 # result0 => true
 
-# target1 = 'skateboard'
-# wordbank1 = ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']
+target1 = 'skateboard'
+wordbank1 = ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']
 # result1 => false
 
-# target2 = ''
-# wordbank2 = ['cat', 'dog', 'mouse']
+target2 = ''
+wordbank2 = ['cat', 'dog', 'mouse']
 # result2 => true
 
+target3 = 'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef'
+wordbank3 = ['ee', 'eeee', 'eeeeeee', 'eeeeeeeeeee', 'eeeeeeerrt']
+# result3 => false
+
+# BRUTE FORCE:
+# M = len of target
+# N = len of word bank
+# time : O(N^M * M)
+# space: O(M^2)
 
 def can_construct(target, word_bank):
-    pass
+    if target == '': return True
 
+    for word in word_bank:
+        if target.find(word) == 0: # if the beginning of word is the 0th index of target,
+            suffix = target[len(word):] # slice the word to get remainder after removing the amount of characters of word
+            if can_construct(suffix, word_bank): return True
+    return False
+
+
+print(can_construct(target0, wordbank0))
+print(can_construct(target1, wordbank1))
+print(can_construct(target2, wordbank2))
+print(can_construct(target3, wordbank3))
 
 
 
