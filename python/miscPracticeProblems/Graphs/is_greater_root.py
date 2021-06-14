193a7ca9f7fe93f5720f2b8ab5f8b41bed84d649
Saul Feliz <646634@gmail.com>
2021-06-14 16:32:04 -0500

    solved is_greater_root.py

--- python/miscPracticeProblems/Graphs/is_greater_root.py
+++ python/miscPracticeProblems/Graphs/is_greater_root.py
@@ -6,20 +6,30 @@
     #       5     12
     #     /   \      \
     #    3      7      14 
-
+    #                /    \
+    #              13       20
 
 from binary_search_tree import BST
 
-T = BST()
-T.insert(10)
-T.insert(5)
-T.insert(12)
-T.insert(3)
-T.insert(7)
-T.insert(14)
+t = BST()
+t.insert(10)
+t.insert(5)
+t.insert(12)
+t.insert(3)
+t.insert(7)
+t.insert(14)
+t.insert(13)
+t.insert(20)
 
-T.print_tree_inOrder()
+tree_root_value = t.root.value
+def is_greater_root(node):
+    if node is None: return 0
+    count = 0
 
-def is_greater_root(T):
-    pass
+    count = is_greater_root(node.left)
+    count += 1
+    count += is_greater_root(node.right)
 
+    return count
+
+print(is_greater_root(t.root.right))
