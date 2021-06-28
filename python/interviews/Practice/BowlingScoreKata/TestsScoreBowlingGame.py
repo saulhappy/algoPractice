6d3002d0d36cf4490fdb49c142fb860b498f72e1
Saul Feliz <646634@gmail.com>
2021-06-28 17:43:25 -0500

    refactor tests. added helper test methods

--- python/interviews/Practice/BowlingScoreKata/TestsScoreBowlingGame.py
+++ python/interviews/Practice/BowlingScoreKata/TestsScoreBowlingGame.py
@@ -1,22 +1,27 @@
+from logging import NullHandler
 import unittest
 from ScoreBowlingGame import ScoreBowlingGame
 
 class TestBowlingGame(unittest.TestCase):
+
+    def setUp(self):
+        self.bowling_game = ScoreBowlingGame()
+
     def test_GameCreate(self):
-         bowling_game = ScoreBowlingGame()
-         self.assertIsInstance(bowling_game, ScoreBowlingGame)
+         self.assertIsInstance(self.bowling_game, ScoreBowlingGame)
 
     def test_GameAllZeros(self):
-        bowling_game = ScoreBowlingGame()
-        for _ in range(20):
-            bowling_game.roll(0)
-        self.assertEquals(bowling_game.score(), 0)
+        self.helper_roll_many(0, 20)
+        self.assertEquals(self.bowling_game.score(), 0)
 
     def test_GameAllOnes(self):
-        bowling_game = ScoreBowlingGame()
-        for _ in range(20):
-            bowling_game.roll(1)
-        self.assertEquals(bowling_game.score(), 20)
+        self.helper_roll_many(1, 20)
+        self.assertEquals(self.bowling_game.score(), 20)
+
+    def helper_roll_many(self, pins_knocked_down, num_rolls):
+        for _ in range(num_rolls):
+            self.bowling_game.roll(pins_knocked_down)
+
 
 
 if __name__ == "__main__":
