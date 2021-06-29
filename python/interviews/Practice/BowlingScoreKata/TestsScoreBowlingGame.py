cec6f9f61d77fb90b824c3039c9847774b3d234b
Saul Feliz <646634@gmail.com>
2021-06-29 08:01:02 -0500

    added first spare test

--- python/interviews/Practice/BowlingScoreKata/TestsScoreBowlingGame.py
+++ python/interviews/Practice/BowlingScoreKata/TestsScoreBowlingGame.py
@@ -5,7 +5,7 @@ from ScoreBowlingGame import ScoreBowlingGame
 class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
-        self.bowling_game = ScoreBowlingGame()
+        self.bowling_game = ScoreBowlingGame() # Q: why doesn't this accumulate scores?
 
     def test_GameCreate(self):
          self.assertIsInstance(self.bowling_game, ScoreBowlingGame)
@@ -17,6 +17,13 @@ class TestBowlingGame(unittest.TestCase):
     def test_GameAllOnes(self):
         self.helper_roll_many(1, 20)
         self.assertEquals(self.bowling_game.score(), 20)
+
+    def test_GameOneSpareThen6ThenZeros(self):
+        self.bowling_game.roll(7)
+        self.bowling_game.roll(3)
+        self.bowling_game.roll(6)
+        self.helper_roll_many(0, 17)
+        self.assertEquals(self.bowling_game.score(), 22)
 
     def helper_roll_many(self, pins_knocked_down, num_rolls):
         for _ in range(num_rolls):
