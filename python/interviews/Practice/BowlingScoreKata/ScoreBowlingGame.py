c5160b9ebd743cd7e23c7b9c6171697fdbf1b472
Saul Feliz <646634@gmail.com>
2021-07-01 23:41:23 -0500

    refactor score method

--- python/interviews/Practice/BowlingScoreKata/ScoreBowlingGame.py
+++ python/interviews/Practice/BowlingScoreKata/ScoreBowlingGame.py
@@ -6,4 +6,16 @@ class ScoreBowlingGame():
         self.rolls.append(pins_knocked)
     
     def score(self):
-        return sum(self.rolls)
+        result = 0
+        roll_index = 0
+        for frame_index in range(10):
+            if self.rolls[roll_index] + self.rolls[roll_index + 1] == 10:
+                result += self.rolls[roll_index] + self.rolls[roll_index + 1] + self.rolls[roll_index + 2]
+            else:
+                result += self.rolls[roll_index] + self.rolls[roll_index + 1]
+            roll_index += 2
+        return result
+
+
+
+        # return sum(self.rolls)
