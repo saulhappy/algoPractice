7543e94a5d8cbf5e4cad6e272b047c7aa5b89fa2
Saul Feliz <646634@gmail.com>
2021-07-11 09:46:18 -0500

    added reverse_vowels.py problem

---
+++ python/miscPracticeProblems/Algorithms/Misc/reverse_vowels.py
@@ -0,0 +1,18 @@
+"""
+source: https://leetcode.com/problems/reverse-vowels-of-a-string/
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both cases.
+
+Example 1:
+
+Input: s = "hello"
+Output: "holle"
+Example 2:
+
+Input: s = "leetcode"
+Output: "leotcede"
+"""
+
+def reverse_vowels(s):
+    pass 
