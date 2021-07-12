a04a1c0a03a1cbaa99d1e9ffc182e750b91499c0
Saul Feliz <646634@gmail.com>
2021-07-12 09:18:07 -0500

    added algorithm

--- python/miscPracticeProblems/Algorithms/Misc/reverse_vowels.py
+++ python/miscPracticeProblems/Algorithms/Misc/reverse_vowels.py
@@ -13,6 +13,14 @@ Example 2:
 Input: s = "leetcode"
 Output: "leotcede"
 """
+"""
+algorithm to try: 
+- create a vowels array with both cases
+- create a vowels stack
+- iterte string, fill stack with the vowels of the string
+- iterate string again: if you see a vowel, pop a vowel out of the stack and replace the char of the string with the vowel just poppped
+return new string
 
+"""
 def reverse_vowels(s):
     pass 
